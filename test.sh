417c527fa7fe10e9f130eb464386654da37bd39b
Sean Law <sean.law@tdameritrade.com>
2017-02-21 09:55:37 -0500

    Tests Passed for Scipy, Numpy convolve

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nosetests
+nosetests --nocapture
--- tests/test_core.py
+++ tests/test_core.py
@@ -25,14 +25,14 @@ class TestCore:
         T = np.array(range(10))
         Q = np.array([3,4,5])
         left = naive_rolling_window_dot_product(Q, T)
-        right = core.sliding_dot_product(Q, T)
+        right = core.numpy_sliding_dot_product(Q, T)
         npt.assert_almost_equal(left, right)
 
     def test_sliding_dot_product_with_range_neg_10(self):
         T = np.negative(np.array(range(10)))
         Q = np.array([3,4,5])
         left = naive_rolling_window_dot_product(Q, T)
-        right = core.sliding_dot_product(Q, T)
+        right = core.numpy_sliding_dot_product(Q, T)
         npt.assert_almost_equal(left, right)
 
     def test_sliding_dot_product_with_random_T50_Q5(self):
@@ -41,13 +41,22 @@ class TestCore:
         # Select 5 random floats in range [-1000, 1000]
         Q = np.random.uniform(-1000, 1000, [5])
         left = naive_rolling_window_dot_product(Q, T)
-        right = core.sliding_dot_product(Q, T)
+        right = core.numpy_sliding_dot_product(Q, T)
         npt.assert_almost_equal(left, right)
 
     def test_sliding_dot_product_with_range_10(self):
-        T = np.array(range(10))
-        Q = np.array([3,4,5])
-        left = np.dot(rolling_window(T, len(Q)), Q)
-        right = core.sliding_dot_product(Q, T)
+       T = np.array(range(10))
+       Q = np.array([3,4,5])
+       left = np.dot(rolling_window(T, len(Q)), Q)
+       right = core.numpy_sliding_dot_product(Q, T)
+       npt.assert_almost_equal(left, right)
+
+    def test_scipy_fftconvolve_sliding_dot_product_with_random_T50_Q5(self):
+        # Select 50 random floats in range [-1000, 1000]
+        T = np.random.uniform(-1000, 1000, [50])
+        # Select 5 random floats in range [-1000, 1000]
+        Q = np.random.uniform(-1000, 1000, [5])
+        left = naive_rolling_window_dot_product(Q, T)
+        right = core.scipy_fftconvolve_sliding_dot_product(Q, T)
         npt.assert_almost_equal(left, right)
 
