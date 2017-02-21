b9357ad19202f6f27e87cfe407d52b0fd5e4d6c9
Sean Law <sean.law@tdameritrade.com>
2017-02-21 10:54:09 -0500

    Made tests a power of 2

--- tests/test_core.py
+++ tests/test_core.py
@@ -16,25 +16,25 @@ class TestCore:
     def tearDown(self):
         pass
 
-    def test_sliding_dot_product_with_range_10(self):
-        T = np.array(range(10))
-        Q = np.array([3,4,5])
+    def test_sliding_dot_product_with_range_16(self):
+        T = np.array(range(16))
+        Q = np.array([3,4,5,6])
         left = naive_rolling_window_dot_product(Q, T)
         right = core.sliding_dot_product(Q, T)
         npt.assert_almost_equal(left, right)
 
-    def test_sliding_dot_product_with_range_neg_10(self):
-        T = np.negative(np.array(range(10)))
-        Q = np.array([3,4,5])
+    def test_sliding_dot_product_with_range_neg_16(self):
+        T = np.negative(np.array(range(16)))
+        Q = np.array([3,4,5,6])
         left = naive_rolling_window_dot_product(Q, T)
         right = core.sliding_dot_product(Q, T)
         npt.assert_almost_equal(left, right)
 
-    def test_sliding_dot_product_with_random_T50_Q5(self):
+    def test_sliding_dot_product_with_random_T64_Q8(self):
         # Select 50 random floats in range [-1000, 1000]
-        T = np.random.uniform(-1000, 1000, [50])
+        T = np.random.uniform(-1000, 1000, [64])
         # Select 5 random floats in range [-1000, 1000]
-        Q = np.random.uniform(-1000, 1000, [5])
+        Q = np.random.uniform(-1000, 1000, [8])
         left = naive_rolling_window_dot_product(Q, T)
         right = core.sliding_dot_product(Q, T)
         npt.assert_almost_equal(left, right)
