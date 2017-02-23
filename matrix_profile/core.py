4161c3e48faf5981654ed616711e8e85bc9db2bc
Sean Law <sean.law@tdameritrade.com>
2017-02-23 11:27:14 -0500

    Moved strided rolling window to test_core.py

--- matrix_profile/core.py
+++ matrix_profile/core.py
@@ -18,11 +18,6 @@ def get_parser():
 
     return parser
 
-def rolling_window(a, window):
-    shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
-    strides = a.strides + (a.strides[-1],)
-    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
-
 def timeit(func):
     """
     Timing decorator
