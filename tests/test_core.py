160dde5b75ac1a176cfd10fb1c6f187ddbe4c2a7
Sean Law <sean.law@tdameritrade.com>
2017-03-04 16:06:28 -0500

    Converted tests to use test generator

--- tests/test_core.py
+++ tests/test_core.py
@@ -9,67 +9,67 @@ def naive_rolling_window_dot_product(Q, T):
         result[i] = np.dot(T[i:i + window], Q)
     return result
 
-def rolling_window(a, window):
-    shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
-    strides = a.strides + (a.strides[-1],)
-    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
-
 def z_norm(x, axis=0):
     return (x - np.mean(x, axis, keepdims=True))/np.std(x, axis, keepdims=True)
 
 class TestCore:
+    def test_generator(self):
+        arrays = [
+                  (np.array(range(5), dtype=np.float64),
+                   np.array([-1,1,2], dtype=np.float64)),
+                  (np.random.uniform(-1000, 1000, [64]),
+                   np.random.uniform(-1000, 1000, [8])),
+                 ]
+        funcs = [f for f in dir(self) if f.startswith('run')]
+        for func in funcs:
+            for T, Q in arrays:
+                yield func, T, Q
+
     def setUp(self):
         pass
 
     def tearDown(self):
         pass
 
-    def test_sliding_dot_product_with_range_16(self):
-        T = np.array(range(16))
-        Q = np.array([3,4,5,6])
+    def run_sliding_dot_product(self, T, Q):
         left = naive_rolling_window_dot_product(Q, T)
         right = core.sliding_dot_product(Q, T)
         npt.assert_almost_equal(left, right)
 
-    def test_sliding_dot_product_with_range_neg_16(self):
-        T = np.negative(np.array(range(16)))
-        Q = np.array([3,4,5,6])
-        left = naive_rolling_window_dot_product(Q, T)
-        right = core.sliding_dot_product(Q, T)
-        npt.assert_almost_equal(left, right)
-
-    def test_sliding_dot_product_with_random_T64_Q8(self):
-        # Select 64 random floats in range [-1000, 1000]
-        T = np.random.uniform(-1000, 1000, [64])
-        # Select 8 random floats in range [-1000, 1000]
-        Q = np.random.uniform(-1000, 1000, [8])
-        left = naive_rolling_window_dot_product(Q, T)
-        right = core.sliding_dot_product(Q, T)
-        npt.assert_almost_equal(left, right)
-
-    def test_compute_mean_std(self):
-        # Select 64 random floats in range [-1000, 1000]
-        T = np.random.uniform(-1000, 1000, [64])
-        # Select 8 random floats in range [-1000, 1000]
-        Q = np.random.uniform(-1000, 1000, [8])
+    def run_compute_mean_std(self, T, Q):
         m = Q.shape[0]
         left_μ_Q = np.sum(Q)/m
-        left_σ_Q = np.sqrt(np.sum(np.square(Q))/m - np.square(left_μ_Q))
-        left_M_T = np.mean(rolling_window(T, m), 1)
-        left_Σ_T = np.std(rolling_window(T,m), 1)
-        right_μ_Q, right_σ_Q, right_M_T, right_Σ_T = core.compute_mean_std(Q, T)
-
+        left_σ_Q = np.sqrt(np.sum(np.square(Q-left_μ_Q)/m))
+        left_M_T = np.mean(core.rolling_window(T,m), axis=1)
+        left_Σ_T = np.std(core.rolling_window(T,m), axis=1)
+        right_μ_Q, right_σ_Q, right_M_T, right_Σ_T = core.compute_mean_std(Q,T)
         npt.assert_almost_equal(left_μ_Q, right_μ_Q)
         npt.assert_almost_equal(left_σ_Q, right_σ_Q)
         npt.assert_almost_equal(left_M_T, right_M_T)
         npt.assert_almost_equal(left_Σ_T, right_Σ_T)
 
-    def test_calculate_distance_profile(self):
-        T = np.array(range(6))
-        Q = np.array(range(3))
+    def run_calculate_distance_profile(self, T, Q):
         m = Q.shape[0]
-        left = np.linalg.norm(z_norm(rolling_window(T,m), 1) - z_norm(Q), axis=1)
-        QT = core.sliding_dot_product(Q, T)
+        left = np.linalg.norm(z_norm(core.rolling_window(T,m), 1) - z_norm(Q), axis=1)
+        QT = core.sliding_dot_product(Q,T)
         μ_Q, σ_Q, M_T, Σ_T = core.compute_mean_std(Q, T)
         right = core.calculate_distance_profile(m, QT, μ_Q, σ_Q, M_T, Σ_T)
+        #right = core.mueen_calculate_distance_profile(Q,T)
         npt.assert_almost_equal(left, right)
+
+    def run_calculate_distance_profile(self, T, Q):
+        m = Q.shape[0]
+        left = np.linalg.norm(z_norm(core.rolling_window(T,m), 1) - z_norm(Q), axis=1)
+        QT = core.sliding_dot_product(Q,T)
+        μ_Q, σ_Q, M_T, Σ_T = core.compute_mean_std(Q, T)
+        right = core.calculate_distance_profile(m, QT, μ_Q, σ_Q, M_T, Σ_T)
+        #right = core.mueen_calculate_distance_profile(Q,T)
+        npt.assert_almost_equal(left, right)
+
+    def run_mass(self, T, Q):
+        m = Q.shape[0]
+        QT = core.sliding_dot_product(Q,T)
+        μ_Q, σ_Q, M_T, Σ_T = core.compute_mean_std(Q, T)
+        #right = core.calculate_distance_profile(m, QT, μ_Q, σ_Q, M_T, Σ_T)
+        #print(left)
+
