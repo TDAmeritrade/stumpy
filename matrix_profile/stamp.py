c4b1103e3373d8fb9799cb86fb850ac66fda6161
Sean Law <sean.law@tdameritrade.com>
2017-03-06 12:40:21 -0500

    Basic stamp test pass. Need to add exclusionary zone

--- matrix_profile/stamp.py
+++ matrix_profile/stamp.py
@@ -1,25 +1,63 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from . import core
+import numpy as np
+from matrix_profile import core
 
-class STAMP(object):
+def check_dtype(arr, dtype=np.float):
+    """
+    Check if array has correct dtype
+    """
+    if not issubclass(arr.dtype.type, dtype):
+        msg = '{} type expected but found {}'.format(dtype, arr.dtype.type)
+        raise TypeError(msg)
 
-    def __init__(self):
-        """
-        """
-        self.T = None
-        self.Q = None
+def stamp(T_A, T_B, m, ignore_trivial=False):
+    """
+    DOI: 10.1109/ICDM.2016.0179
+    See Table III
 
-    def get_matrix_profile(self):
-        """
-        """
-        return
+    Timeseries, T_B, will be annotated with the distance location
+    (or index) of all its subsequences in another times series, T_A.
+
+    Return: For every subsequence, Q, in T_B, you will get a distance
+    and index for the closest subsequence in T_A. Thus, the array
+    returned will have length T_B.shape[0]-m+1
+    """
+    check_dtype(T_A)
+    check_dtype(T_B)
+    subseq_T_B = core.rolling_window(T_B, m)
+
+    # Create new function that calls MASS and handles idx, profile
+    def subseq_mass(Q, T, trivial_idx=None):
+        D = core.mass(Q, T)
+        if trivial_idx is not None:
+            D[trivial_idx] = np.inf
+        # Element-wise Min
+        I = np.argmin(D)
+        P = D[I]
+        return P, I
+
+    # Check if T_A and T_B are the same (i.e., self-join)
+
+    # Add exclusionary zone
+    if ignore_trivial:
+        out = [subseq_mass(subseq, T_A, i) for i, subseq in enumerate(subseq_T_B)]
+    else:
+        out = [subseq_mass(subseq, T_A) for subseq in subseq_T_B]
+    out = np.array(out, dtype=object)
+    
+    return out
 
 
 if __name__ == '__main__':
     core.check_python_version()
-    parser = core.get_parser()
-    args = parser.parse_args()
-    mp = STAMP()
-    mp.get_matrix_profile()
+    #parser = get_parser()
+    #args = parser.parse_args()
+    #N = 17279800  # GPU-STOMP Comparison
+    N = 12*24*365  # Every 5 minutes: 12 times an hour, 24 hours, 365 days
+    M = 12*24
+    # Select 50 random floats in range [-1000, 1000]
+    T = np.random.uniform(-1000, 1000, [N])
+    # Select 5 random floats in range [-1000, 1000]
+    Q = np.random.uniform(-1000, 1000, [M])
--- tests/test_stamp.py
+++ tests/test_stamp.py
@@ -1,4 +1,66 @@
-from matrix_profile import stamp
+import os
+import numpy as np
+import numpy.testing as npt
+from matrix_profile import stamp, core
+from functools import partial
+import os
 
-def test_nothing():
-    pass
+# From Matrix Profile Authors
+TEST_DATA = os.path.join(os.path.dirname(__file__), 'testData.txt')
+TEST_INP_INDICES = os.path.join(os.path.dirname(__file__), 'testIdx.txt')
+TEST_MATRIX_PROFILE = os.path.join(os.path.dirname(__file__), 'testMP.txt')
+TEST_OUT_INDICES = os.path.join(os.path.dirname(__file__), 'testMPI.txt')
+
+def naive_mass(Q, T, m, trivial_idx=None):
+    D = np.linalg.norm(core.z_norm(core.rolling_window(T, m), 1) - core.z_norm(Q), axis=1)
+    if trivial_idx is not None:
+            D[trivial_idx] = np.inf
+    I = np.argmin(D)
+    P = D[I]
+    return P, I
+
+
+class TestStamp:
+    def test_generator(self):
+        test_dir = os.path.basename(os.path.dirname(__file__))
+        cls = self.__class__.__name__
+        base = os.path.basename(__file__)
+        name = os.path.splitext(base)[0]
+        arrays = [
+                  (np.array([9,8100,-60,7], dtype=np.float64),
+                   np.array([584,-11,23,79,1001], dtype=np.float64)),
+                  (np.random.uniform(-1000, 1000, [8]),
+                   np.random.uniform(-1000, 1000, [64])),
+                 ]
+        funcs = [f for f in dir(self) if f.startswith('run')]
+        for func in funcs:
+            for i, (Q, T) in enumerate(arrays):
+                f = partial(getattr(self, func), Q, T)
+                f.description = '{}.{}.{}.{} {}'.format(test_dir, name, cls, func, i+1) 
+                yield f
+
+    def setUp(self):
+        self.test_data = np.loadtxt(TEST_DATA, dtype=np.float64, skiprows=2)
+        self.test_inp_indices = np.loadtxt(TEST_INP_INDICES, dtype=np.int32, skiprows=1)
+        self.test_inp_indices = self.test_inp_indices - 1  # Fix off-by-one index
+        # Reorder test data with input indices from Matrix Profile Authors
+        self.test_data = self.test_data[self.test_inp_indices]  # Reorder test data with input indices
+
+        self.test_matrix_profile = np.loadtxt(TEST_MATRIX_PROFILE, dtype=np.float64, skiprows=0)
+        self.test_out_indices = np.loadtxt(TEST_OUT_INDICES, dtype=np.int32, skiprows=1)
+        self.test_out_indices = self.test_out_indices - 1  # Fix off-by-one index
+
+    def tearDown(self):
+        pass
+
+    def run_stamp_self_join(self, T_A, T_B):
+        m = 3
+        left = np.array([naive_mass(Q, T_B, m, i) for i, Q in enumerate(core.rolling_window(T_B, m))], dtype=object)
+        right = stamp.stamp(T_B, T_B, m, ignore_trivial=True)
+        npt.assert_almost_equal(left, right)
+
+    def run_stamp_A_B_join(self, T_A, T_B):
+        m = 3
+        left = np.array([naive_mass(Q, T_A, m) for Q in core.rolling_window(T_B, m)])
+        right = stamp.stamp(T_A, T_B, m)
+        npt.assert_almost_equal(left, right)
