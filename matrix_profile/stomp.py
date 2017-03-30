b4cc8caa685b73e4f4ca9a41961f4f04dc79d314
Sean Law <sean.law@tdameritrade.com>
2017-03-29 20:41:12 -0400

    QT calculated correctly for different length T_A, T_B

--- matrix_profile/stomp.py
+++ matrix_profile/stomp.py
@@ -40,25 +40,27 @@ def stomp(T_A, T_B, m, ignore_trivial=False):
     else:
         out[0] = stamp.mass(T_A[:m], T_B, M_T, Σ_T)
 
-    #k = QT.shape[0]-1
-    # for i in range(1, l):
-    #     #QT[1:] = QT[:k]- T_B[i-1]*T_A[:k] + T_B[i-1+k]*T_A[-k:]
-    #     QT[1:] = QT[:k]- T_B[i-1]*T_A[:k] + T_B[i-1+k]*T_A[-k:]
-    #     QT[0] = QT_first[i]
-    #     D = core.calculate_distance_profile(m, QT, np.array([μ_Q[i]]), np.array([σ_Q[i]]), M_T, Σ_T)
-    #     I = np.argmin(D)
-    #     P = D[I]
-    #     out[i] = P, I
-
     k = T_A.shape[0]-m+1
     for i in range(1, l):
-        for j in range(k-1,0,-1):
-            QT[j] = QT[j-1] - T_B[i-1]*T_A[j-1] + T_B[i+m-1]*T_A[j+m-1]
-        QT[0] =QT_first[i]
-        D = core.calculate_distance_profile(m, QT, np.array([μ_Q[i]]), np.array([σ_Q[i]]), M_T, Σ_T)
+        QT[1:] = QT[:k-1] - T_B[i-1]*T_A[:k-1] + T_B[i-1+m]*T_A[-(k-1):]
+        QT[0] = QT_first[i]
+        print(M_T.shape, Σ_T.shape, μ_Q[i].shape, σ_Q[i].shape, QT.shape)
+        D = core.calculate_distance_profile(m, QT, μ_Q[i], σ_Q[i], M_T, Σ_T)
         I = np.argmin(D)
         P = D[I]
         out[i] = P, I
+
+    # k = T_A.shape[0]-m+1
+    # for i in range(1, l):
+    #     for j in range(k-1,0,-1):
+    #         # Ensure QT calculation works for any T_A, T_B of different lengths
+    #         QT[j] = QT[j-1] - T_B[i-1]*T_A[j-1] + T_B[i+m-1]*T_A[j+m-1]
+    #     QT[0] =QT_first[i]
+    #     print(QT)
+    #     #D = core.calculate_distance_profile(m, QT, np.array([μ_Q[i]]), np.array([σ_Q[i]]), M_T, Σ_T)
+    #     #I = np.argmin(D)
+    #     #P = D[I]
+    #     #out[i] = P, I
     out = np.array(out, dtype=object)
     
     return out
