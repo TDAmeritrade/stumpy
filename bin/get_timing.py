7e9fd604c8ab045749fe21ecaa7512fb60b4ee22
Sean Law <sean.law@tdameritrade.com>
2019-03-02 16:59:47 -0600

    Modified timing criteria and output

--- bin/get_timing.py
+++ bin/get_timing.py
@@ -4,11 +4,25 @@ import matrix_profile as mp
 import numpy as np
 from timeit import default_timer as timer
 
-for i in range(3, 7):
-    n = 10**i
-    x = np.random.rand(n)
+def get_human_readable_time(total_time):
+        hours, rem = divmod(total_time, 3600)
+        minutes, seconds = divmod(rem, 60)
+        hours = int(hours)
+        minutes = int(minutes)
 
-    start = timer()
+        return f"{hours:0>2}:{minutes:0>2}:{seconds:05.2f}"
+
+if __name__ == '__main__':
+    # Makes sure stump function is already jit compiled
+    x = np.random.rand(51)
     mp.stump.stump(x, x, 50, ignore_trivial=True)
-    end = timer()
-    print(f'n: {n}  time: {end-start}')
+
+    for i in range(6, 25):
+        n = 2**i
+        x = np.random.rand(n)
+
+        start = timer()
+        mp.stump.stump(x, x, 50, ignore_trivial=True)
+        end = timer()
+        elapsed_time = get_human_readable_time(end-start)
+        print(f'i: {i}  n: {n}  time: {elapsed_time}')
