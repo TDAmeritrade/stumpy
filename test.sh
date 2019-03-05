a991f87ea9ca3b6688b35e88ac49a8b051e05a35
Sean Law <seanmylaw@gmail.com>
2019-03-04 19:26:28 -0500

    Added parallel numba tests for performance

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+py.test --capture=sys -s tests/test_stump.py
+
 export NUMBA_DISABLE_JIT=1
 SITE_PACKAGES_DIR=`pip show matrix_profile | grep Location | awk '{print $2}'`
 py.test --cov=$SITE_PACKAGES_DIR/matrix_profile --cov-report term-missing tests/ --capture=sys -s
