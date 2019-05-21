504aabd3dd87145aca865bddeef05446cbae8cb7
Will.Li <will.li@iress.com>
2019-05-21 21:21:02 +1000

    try azure bash

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
+#!/bin/sh
+
 echo "Testing Numba JIT Compiled Functions"
 py.test -W ignore::RuntimeWarning -W ignore::DeprecationWarning tests/test_stump.py tests/test_stumped.py tests/test_mstump.py tests/test_mstumped.py
 
 echo "Disabling Numba JIT Compiled Functions"
-# export NUMBA_DISABLE_JIT=1
-$env:NUMBA_DISABLE_JIT = 1
+export NUMBA_DISABLE_JIT=1
 
 echo "Testing Python Functions"
 py.test -W ignore::RuntimeWarning -W ignore::DeprecationWarning tests
