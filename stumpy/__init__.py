25192e62e86cc7db745981a573bedf609e5ec97e
Sean Law <seanmylaw@gmail.com>
2019-05-29 22:00:35 -0400

    Ignore raise in __init__

--- stumpy/__init__.py
+++ stumpy/__init__.py
@@ -28,7 +28,7 @@ try:
     here = os.path.normcase(__file__)
     if not here.startswith(os.path.join(dist_loc, "stumpy")):
         # not installed, but there is another version that *is*
-        raise DistributionNotFound
+        raise DistributionNotFound  # pragma: no cover
 except DistributionNotFound:  # pragma: no cover
     __version__ = "Please install this project with setup.py"
 else:  # pragma: no cover
