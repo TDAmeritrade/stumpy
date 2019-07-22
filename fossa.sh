bb1b7b6140e3fb5bf018bafadd07ee2d31d5c690
fat624 <brett.fattori@tdameritrade.com>
2019-07-22 14:19:12 -0400

    Updating script, splitting out into separate shell

---
+++ fossa.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Install latest FOSSA
+curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/fossas/fossa-cli/master/install.sh | bash
+
+# Analyze the project
+FOSSA_API_KEY=59a5cb358c757b5be7c2ed677b2f852b fossa -analyze
