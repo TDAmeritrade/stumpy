22a5cfbc9e9c9fc325fe05d439077bc8ac41bffa
fat624 <brett.fattori@tdameritrade.com>
2019-07-22 14:47:07 -0400

    Try moving FOSSA into a local dir so we have perms to modify exec

--- fossa.sh
+++ fossa.sh
@@ -3,5 +3,12 @@
 # Install latest FOSSA cli
 curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/fossas/fossa-cli/master/install.sh | bash
 
+# Change the execution location
+mkdir fossa-cli
+export BINDIR=./fossa-cli
+
+# API Key
+export FOSSA_API_KEY=59a5cb358c757b5be7c2ed677b2f852b
+
 # Analyze the project
-FOSSA_API_KEY=59a5cb358c757b5be7c2ed677b2f852b fossa -analyze
+./fossa-cli/fossa -analyze
