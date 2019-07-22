7035a63015384ae510dc93b8bef63911e3f2b648
fat624 <brett.fattori@tdameritrade.com>
2019-07-22 14:55:11 -0400

    Move BINDIR export so it's picked up by the installer script

--- fossa.sh
+++ fossa.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# Install latest FOSSA cli
-curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/fossas/fossa-cli/master/install.sh | bash
-
 # Change the execution location
 mkdir fossa-cli
 export BINDIR=./fossa-cli
+
+# Install latest FOSSA cli
+curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/fossas/fossa-cli/master/install.sh | bash
 
 # API Key
 export FOSSA_API_KEY=59a5cb358c757b5be7c2ed677b2f852b
