d19949ccf8fd2da3f68d93e97ccbf8c051910fc8
fat624 <brett.fattori@tdameritrade.com>
2019-07-23 10:04:32 -0400

    Modified azure yaml so its output is better Update fossa.sh permissions to be executable

--- fossa.sh
+++ fossa.sh
@@ -7,5 +7,5 @@ export BINDIR=./fossa-cli
 # Install latest FOSSA cli
 curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/fossas/fossa-cli/master/install.sh | bash
 
-# Analyze the project
+# Analyze the project and submit to FOSSA
 FOSSA_API_KEY=$1 ./fossa-cli/fossa analyze
