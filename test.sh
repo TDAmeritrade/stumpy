c7e3176ddb545bab03d4ce295af6f1946158e902
Sean Law <seanmylaw@gmail.com>
2019-07-23 11:36:01 -0400

    Split JIT tests to ensure proper dask teardown

--- test.sh
+++ test.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 echo "Testing Numba JIT Compiled Functions"
-py.test -W ignore::RuntimeWarning -W ignore::DeprecationWarning tests/test_stump.py tests/test_stumped.py tests/test_mstump.py tests/test_mstumped.py
+py.test -W ignore::RuntimeWarning -W ignore::DeprecationWarning tests/test_stump.py
+echo "Testing Numba JIT Compiled Functions"
+py.test -W ignore::RuntimeWarning -W ignore::DeprecationWarning tests/test_stumped.py
+echo "Testing Numba JIT Compiled Functions"
+py.test -W ignore::RuntimeWarning -W ignore::DeprecationWarning tests/test_mstump.py
+echo "Testing Numba JIT Compiled Functions"
+py.test -W ignore::RuntimeWarning -W ignore::DeprecationWarning tests/test_mstumped.py
 
 echo "Disabling Numba JIT Compiled Functions"
 export NUMBA_DISABLE_JIT=1
