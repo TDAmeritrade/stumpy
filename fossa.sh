e712cb1645927e7040c9c97dc5f6b311ad05e087
fat624 <brett.fattori@tdameritrade.com>
2019-07-23 09:55:21 -0400

    Fixed #42

---
+++ fossa.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Change the execution location
+mkdir fossa-cli
+export BINDIR=./fossa-cli
+
+# Install latest FOSSA cli
+curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/fossas/fossa-cli/master/install.sh | bash
+
+# Analyze the project
+FOSSA_API_KEY=$1 ./fossa-cli/fossa analyze
