0bb854c13dbe7808b21ca94728d911b880972597
Sean Law <seanmylaw@gmail.com>
2019-08-03 19:57:29 -0400

    Removed empty cells from Tutorials

--- pypi.sh
+++ pypi.sh
@@ -2,7 +2,18 @@
 
 # 1. Update version number in setup.py
 # 2. Update CHANGELOG
-# 3. conda-forge should update itself automatically
+#
+# For conda-forge
+# 1. Fork the stumpy-feedstock
+# 2. Create a new branch for the new version
+# 3. In the recipe/meta.yaml file
+#    a) Update version number on line 2
+#    b) Update the sha256 on line 10 according to what is found on PyPI
+#       in the "Download files" section of the left navigation pane
+#    c) Reset the build number on line 14 since this is a new version
+# 4. Commit the changes and push upstream for a PR
+# 5. Check the checkboxes in the PR
+# 6. Add a comment with "@conda-forge-admin, please rerender"
 #
 # To check that the distribution is valid, execute:
 # twine check dist/* 
