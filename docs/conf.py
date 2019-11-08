4de059efe13fdcdeac872f0f47794557cb2e9e9e
Sean Law <seanmylaw@gmail.com>
2019-11-08 11:20:31 -0500

    Updated formatting and added newline before logo

--- docs/conf.py
+++ docs/conf.py
@@ -103,9 +103,7 @@ html_static_path = ["_static"]
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
-html_css_files = [
-    'css/custom.css',
-]
+html_css_files = ["css/custom.css"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
