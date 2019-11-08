e41839561a280a190f48884a81f1561e5a174e11
Sean Law <seanmylaw@gmail.com>
2019-11-08 10:54:59 -0500

    Added custom css to sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -101,6 +101,12 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
