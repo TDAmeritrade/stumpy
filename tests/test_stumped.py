1fd81a2ae27a3c19cfb73eaa46e21783e49a8ac1
Sean Law <seanmylaw@gmail.com>
2020-01-12 21:34:36 -0500

    Temporary unswap of input

--- tests/test_stumped.py
+++ tests/test_stumped.py
@@ -14,8 +14,8 @@ def dask_client():
     client = Client(cluster)
     yield client
     # teardown
-    cluster.close()
     client.close()
+    cluster.close()
 
 
 test_data = [
@@ -285,13 +285,13 @@ def test_constant_subsequence_A_B_join_df(dask_client):
 @pytest.mark.filterwarnings("ignore:numpy.ndarray size changed")
 @pytest.mark.filterwarnings("ignore:\\s+Port 8787 is already in use:UserWarning")
 def test_two_constant_subsequences_A_B_join_swap(dask_client):
-    T_A = np.array([0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1], dtype=np.float64)
+    T_A = np.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1], dtype=np.float64)
     T_B = np.concatenate((np.zeros(20, dtype=np.float64), np.ones(5, dtype=np.float64)))
     m = 3
     left = np.array(
-        [utils.naive_mass(Q, T_B, m) for Q in core.rolling_window(T_A, m)], dtype=object
+        [utils.naive_mass(Q, T_A, m) for Q in core.rolling_window(T_B, m)], dtype=object
     )
-    right = stumped(dask_client, T_B, m, T_A, ignore_trivial=False)
+    right = stumped(dask_client, T_A, m, T_B, ignore_trivial=False)
     utils.replace_inf(left)
     utils.replace_inf(right)
     npt.assert_almost_equal(left[:, 0], right[:, 0])  # ignore indices
