8bd05b4bc11abb6ccfc13cd415ba6dccb798d9aa
Sean Law <seanmylaw@gmail.com>
2020-04-10 13:43:38 -0400

    Split tests into two separate test files

--- test.sh
+++ test.sh
@@ -26,7 +26,7 @@ echo "Testing Numba JIT Compiled Functions"
 py.test -rsx -W ignore::RuntimeWarning -W ignore::DeprecationWarning tests/test_gpu_stump.py
 py.test -x -W ignore::RuntimeWarning -W ignore::DeprecationWarning tests/test_stump.py tests/test_mstump.py
 check_errs $?
-py.test -x -W ignore::RuntimeWarning -W ignore::DeprecationWarning tests/test_stumped.py tests/test_stumped_one_constant_subsequence.py tests/test_stumped_two_constant_subsequences.py tests/test_mstumped.py
+py.test -x -W ignore::RuntimeWarning -W ignore::DeprecationWarning tests/test_stumped.py tests/test_stumped_one_constant_subsequence.py tests/test_stumped_two_constant_subsequences.py tests/test_stumped_two_constant_subsequences_swap.py tests/test_mstumped.py
 check_errs $?
 py.test -x -W ignore::RuntimeWarning -W ignore::DeprecationWarning tests/test_stumped_one_subsequence_nan_self_join.py tests/test_stumped_one_subsequence_inf_self_join.py tests/test_stumped_one_subsequence_nan_A_B_join.py tests/test_stumped_one_subsequence_inf_A_B_join.py
 check_errs $?
--- tests/test_stumped_two_constant_subsequences_swap.py
+++ tests/test_stumped_two_constant_subsequences_swap.py
@@ -24,16 +24,16 @@ def dask_client():
 @pytest.mark.filterwarnings("ignore:numpy.ufunc size changed")
 @pytest.mark.filterwarnings("ignore:numpy.ndarray size changed")
 @pytest.mark.filterwarnings("ignore:\\s+Port 8787 is already in use:UserWarning")
-def test_two_constant_subsequences_A_B_join(dask_client):
+def test_two_constant_subsequences_A_B_join_swap(dask_client):
     T_A = np.concatenate(
         (np.zeros(10, dtype=np.float64), np.ones(10, dtype=np.float64))
     )
     T_B = np.concatenate((np.zeros(20, dtype=np.float64), np.ones(5, dtype=np.float64)))
     m = 3
     left = np.array(
-        [utils.naive_mass(Q, T_A, m) for Q in core.rolling_window(T_B, m)], dtype=object
+        [utils.naive_mass(Q, T_B, m) for Q in core.rolling_window(T_A, m)], dtype=object
     )
-    right = stumped(dask_client, T_A, m, T_B, ignore_trivial=False)
+    right = stumped(dask_client, T_B, m, T_A, ignore_trivial=False)
     utils.replace_inf(left)
     utils.replace_inf(right)
     npt.assert_almost_equal(left[:, 0], right[:, 0])  # ignore indices
@@ -45,17 +45,17 @@ def test_two_constant_subsequences_A_B_join(dask_client):
 @pytest.mark.filterwarnings("ignore:numpy.ufunc size changed")
 @pytest.mark.filterwarnings("ignore:numpy.ndarray size changed")
 @pytest.mark.filterwarnings("ignore:\\s+Port 8787 is already in use:UserWarning")
-def test_two_constant_subsequences_A_B_join_df(dask_client):
+def test_constant_subsequence_A_B_join_df_swap(dask_client):
     T_A = np.concatenate(
         (np.zeros(10, dtype=np.float64), np.ones(10, dtype=np.float64))
     )
     T_B = np.concatenate((np.zeros(20, dtype=np.float64), np.ones(5, dtype=np.float64)))
     m = 3
     left = np.array(
-        [utils.naive_mass(Q, T_A, m) for Q in core.rolling_window(T_B, m)], dtype=object
+        [utils.naive_mass(Q, T_B, m) for Q in core.rolling_window(T_A, m)], dtype=object
     )
     right = stumped(
-        dask_client, pd.Series(T_A), m, pd.Series(T_B), ignore_trivial=False
+        dask_client, pd.Series(T_B), m, pd.Series(T_A), ignore_trivial=False
     )
     utils.replace_inf(left)
     utils.replace_inf(right)
