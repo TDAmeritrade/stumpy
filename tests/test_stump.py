9464a17eb9e635d71a7dc65a5dc6349497be03b9
Max Mihailescu <mihailescu.m@gmx.de>
2020-05-13 09:29:35 +0200

    Refactored tests and corrected scrump index

--- tests/test_stump.py
+++ tests/test_stump.py
@@ -25,13 +25,7 @@ substitution_values = [np.nan, np.inf]
 def test_stump_self_join(T_A, T_B):
     m = 3
     zone = int(np.ceil(m / 4))
-    left = np.array(
-        [
-            utils.naive_mass(Q, T_B, m, i, zone, True)
-            for i, Q in enumerate(core.rolling_window(T_B, m))
-        ],
-        dtype=object,
-    )
+    left = utils.naive_stamp(T_B, m, exclusion_zone=zone)
     right = stump(T_B, m, ignore_trivial=True)
     utils.replace_inf(left)
     utils.replace_inf(right)
@@ -45,9 +39,7 @@ def test_stump_self_join(T_A, T_B):
 @pytest.mark.parametrize("T_A, T_B", test_data)
 def test_stump_A_B_join(T_A, T_B):
     m = 3
-    left = np.array(
-        [utils.naive_mass(Q, T_A, m) for Q in core.rolling_window(T_B, m)], dtype=object
-    )
+    left = utils.naive_stamp(T_A, m, T_B=T_B)
     right = stump(T_A, m, T_B, ignore_trivial=False)
     utils.replace_inf(left)
     utils.replace_inf(right)
@@ -62,13 +54,7 @@ def test_stump_constant_subsequence_self_join():
     T_A = np.concatenate((np.zeros(20, dtype=np.float64), np.ones(5, dtype=np.float64)))
     m = 3
     zone = int(np.ceil(m / 4))
-    left = np.array(
-        [
-            utils.naive_mass(Q, T_A, m, i, zone, True)
-            for i, Q in enumerate(core.rolling_window(T_A, m))
-        ],
-        dtype=object,
-    )
+    left = utils.naive_stamp(T_A, m, exclusion_zone=zone)
     right = stump(T_A, m, ignore_trivial=True)
     utils.replace_inf(left)
     utils.replace_inf(right)
@@ -83,9 +69,7 @@ def test_stump_one_constant_subsequence_A_B_join():
     T_A = np.random.rand(20)
     T_B = np.concatenate((np.zeros(20, dtype=np.float64), np.ones(5, dtype=np.float64)))
     m = 3
-    left = np.array(
-        [utils.naive_mass(Q, T_A, m) for Q in core.rolling_window(T_B, m)], dtype=object
-    )
+    left = utils.naive_stamp(T_A, m, T_B=T_B)
     right = stump(T_A, m, T_B, ignore_trivial=False)
     utils.replace_inf(left)
     utils.replace_inf(right)
@@ -96,9 +80,7 @@ def test_stump_one_constant_subsequence_A_B_join():
     npt.assert_almost_equal(left[:, 0], right[:, 0])  # ignore indices
 
     # Swap inputs
-    left = np.array(
-        [utils.naive_mass(Q, T_B, m) for Q in core.rolling_window(T_A, m)], dtype=object
-    )
+    left = utils.naive_stamp(T_B, m, T_B=T_A)
     right = stump(T_B, m, T_A, ignore_trivial=False)
     utils.replace_inf(left)
     utils.replace_inf(right)
@@ -111,9 +93,7 @@ def test_stump_two_constant_subsequences_A_B_join():
     )
     T_B = np.concatenate((np.zeros(20, dtype=np.float64), np.ones(5, dtype=np.float64)))
     m = 3
-    left = np.array(
-        [utils.naive_mass(Q, T_A, m) for Q in core.rolling_window(T_B, m)], dtype=object
-    )
+    left = utils.naive_stamp(T_A, m, T_B=T_B)
     right = stump(T_A, m, T_B, ignore_trivial=False)
     utils.replace_inf(left)
     utils.replace_inf(right)
@@ -124,9 +104,7 @@ def test_stump_two_constant_subsequences_A_B_join():
     npt.assert_almost_equal(left[:, 0], right[:, 0])  # ignore indices
 
     # Swap inputs
-    left = np.array(
-        [utils.naive_mass(Q, T_B, m) for Q in core.rolling_window(T_A, m)], dtype=object
-    )
+    left = utils.naive_stamp(T_B, m, T_B=T_A)
     right = stump(T_B, m, T_A, ignore_trivial=False)
     utils.replace_inf(left)
     utils.replace_inf(right)
@@ -150,13 +128,7 @@ def test_stump_nan_inf_self_join(T_A, T_B, substitute_B, substitution_locations)
         T_B_sub[substitution_location_B] = substitute_B
 
         zone = int(np.ceil(m / 4))
-        left = np.array(
-            [
-                utils.naive_mass(Q, T_B_sub, m, i, zone, True)
-                for i, Q in enumerate(core.rolling_window(T_B_sub, m))
-            ],
-            dtype=object,
-        )
+        left = utils.naive_stamp(T_B_sub, m, exclusion_zone=zone)
         right = stump(T_B_sub, m, ignore_trivial=True)
         utils.replace_inf(left)
         utils.replace_inf(right)
@@ -186,13 +158,7 @@ def test_stump_nan_inf_A_B_join(
             T_A_sub[substitution_location_A] = substitute_A
             T_B_sub[substitution_location_B] = substitute_B
 
-            left = np.array(
-                [
-                    utils.naive_mass(Q, T_A_sub, m)
-                    for Q in core.rolling_window(T_B_sub, m)
-                ],
-                dtype=object,
-            )
+            left = utils.naive_stamp(T_A_sub, m, T_B=T_B_sub)
             right = stump(T_A_sub, m, T_B_sub, ignore_trivial=False)
             utils.replace_inf(left)
             utils.replace_inf(right)
@@ -210,13 +176,7 @@ def test_stump_nan_zero_mean_self_join():
     m = 3
 
     zone = int(np.ceil(m / 4))
-    left = np.array(
-        [
-            utils.naive_mass(Q, T, m, i, zone, True)
-            for i, Q in enumerate(core.rolling_window(T, m))
-        ],
-        dtype=object,
-    )
+    left = utils.naive_stamp(T, m, exclusion_zone=zone)
     right = stump(T, m, ignore_trivial=True)
 
     utils.replace_inf(left)
