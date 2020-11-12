1778d0fb1456e345d9eae80045e04f51a22ccba3
Daniel Hähnke <haehnke@gmail.com>
2020-11-12 14:39:38 +0100

    Fix bug in naive implementation
    Bug had been copied from paper's pseudocode (table 1, line 8)

--- tests/test_ostinato.py
+++ tests/test_ostinato.py
@@ -1,8 +1,18 @@
 import numpy as np
 import numpy.testing as npt
 import stumpy
+import pytest
 
 def naive_consensus_search(ts, m):
+    """
+    Brute force consensus motif from
+    <https://www.cs.ucr.edu/~eamonn/consensus_Motif_ICDM_Long_version.pdf>
+
+    See Table 1
+
+    Note that there is a bug in the pseudocode at line 8 where `i` should be `j`.
+    This implementation fixes it.
+    """
     k = len(ts)
 
     rad = np.inf
@@ -19,19 +29,25 @@ def naive_consensus_search(ts, m):
         min_rad = radii[min_rad_index]
         if min_rad < rad:
             rad = min_rad
-            tsind = i
+            tsind = j
             ssind = min_rad_index
 
     return rad, tsind, ssind
 
 
-def test_ostinato():
+@pytest.mark.parametrize('seed', np.arange(100))
+def test_ostinato(seed):
     m = 50
+    np.random.seed(seed)
     ts = [np.random.rand(n) for n in [64, 128, 256]]
 
     rad_naive, tsind_naive, ssind_naive = naive_consensus_search(ts, m)
     rad_ostinato, tsind_ostinato, ssind_ostinato = stumpy.ostinato(ts, m)
 
     npt.assert_almost_equal(rad_naive, rad_ostinato)
+    npt.assert_almost_equal(
+            ts[tsind_naive][ssind_naive:ssind_naive+m],
+            ts[tsind_ostinato][ssind_ostinato:ssind_ostinato+m],
+            )
     npt.assert_almost_equal(tsind_naive, tsind_ostinato)
     npt.assert_almost_equal(ssind_naive, ssind_ostinato)
