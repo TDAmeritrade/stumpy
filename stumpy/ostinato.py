309b8d840b461e28a406278d20bbae75bee7bb91
Daniel Hähnke <haehnke@gmail.com>
2020-11-18 10:03:55 +0100

    Complete _get_central_motif

--- stumpy/ostinato.py
+++ stumpy/ostinato.py
@@ -80,5 +80,66 @@ def ostinato(tss, m):
     return bsf_rad, ts_ind, ss_ind
 
 
-def _get_central_motif(ts, rad, tsind, ssind, m):
-    return
+def _get_central_motif(ts, rad, tsi, ssi, m):
+    """
+    Compare subsequences with the same radius and return the most central motif
+
+    Parameters
+    ----------
+    ts : list
+        List of time series for which to find the most central motif
+
+    rad : float
+        Best radius found by a consensus search algorithm
+
+    tsi : int
+        Index of time series in which `rad` was found first
+
+    ssi : int
+        Start index of subsequence in `tsi` that has radius `rad`
+
+    m : int
+        Window size
+
+    Returns
+    -------
+    rad : float
+        Radius of the most central consensus motif
+
+    tsi : int
+        Index of time series which contains the most central consensus motif
+
+    ssi : int
+        Start index of most central consensus motif within the time series `tsi`
+        that contains it
+    """
+    k = len(ts)
+    # Get nearest neighbors for ostinato hit
+    nn_ost, d_ost = _across_series_nearest_neighbors(ts, tsi, ssi, m)
+    # Alternative candidates with same radius
+    tsi_alt = np.flatnonzero(np.isclose(d_ost, rad))
+    ssi_alt = nn_ost[tsi_alt]
+    num_alt = len(tsi_alt)
+    d_alt = np.zeros((num_alt, k), dtype=float)
+    for i in range(num_alt):
+        _, d_alt[i] = _across_series_nearest_neighbors(ts, tsi_alt[i], ssi_alt[i], m)
+    d_alt_sum = np.sum(d_alt, axis=1)
+    if np.any(d_alt_sum < d_ost.sum()):
+        i_alt_best = np.argmin(d_alt_sum)
+        return rad, tsi_alt[i_alt_best], ssi_alt[i_alt_best]
+    else:
+        return rad, tsi, ssi
+
+
+def _across_series_nearest_neighbors(ts, qtsind, qssind, m):
+    k = len(ts)
+    q = ts[qtsind][qssind : qssind + m]
+    d = np.zeros(k, dtype=float)
+    nn = np.zeros(k, dtype=int)
+    nn[qtsind] = qssind
+    for i in range(k):
+        if i != qtsind:
+            dp = core.mass(q, ts[i])
+            nn[i] = np.argmin(dp)
+            d[i] = dp[nn[i]]
+    return nn, d
