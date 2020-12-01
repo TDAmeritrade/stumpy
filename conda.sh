caba9f582d360264e29c9b2c52d9bc2f21153c1d
Sean Law <seanmylaw@gmail.com>
2020-12-01 08:34:50 -0500

    Added mamba to speed up Python env installation

--- conda.sh
+++ conda.sh
@@ -2,9 +2,12 @@
 
 conda update -y conda
 conda update -y --all
-conda env update --file environment.yml
+conda install -y -c conda-forge mamba
+# conda env update --file environment.yml
+mamba env update --file environment.yml
 if [[ `uname` == "Linux" && `which nvcc | wc -l` -lt "1" ]]; then
-    conda install -y -c conda-forge cudatoolkit-dev
+    # conda install -y -c conda-forge cudatoolkit-dev
+    mamba install -y -c conda-forge cudatoolkit-dev
 fi
 #conda install -y -c conda-forge numpy scipy numba pandas flake8 flake8-docstrings black pytest-cov
 #conda install -y -c conda-forge dask distributed
