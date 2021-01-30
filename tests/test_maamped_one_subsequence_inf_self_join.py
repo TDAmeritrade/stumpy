70e7ab37490bd7cf4e6443bfa35144c4f14f4cdb
Sean Law <seanmylaw@gmail.com>
2021-01-29 23:31:12 -0500

    Changed mstumped to maamped

--- tests/test_maamped_one_subsequence_inf_self_join.py
+++ tests/test_maamped_one_subsequence_inf_self_join.py
@@ -1,7 +1,7 @@
 import numpy as np
 import numpy.testing as npt
 import pandas as pd
-from stumpy import core, mstumped
+from stumpy import core, maamped
 import pytest
 from dask.distributed import Client, LocalCluster
 import warnings
@@ -26,7 +26,7 @@ substitution_locations = [slice(0, 0), 0, -1, slice(1, 3), [0, 3]]
 @pytest.mark.filterwarnings("ignore:\\s+Port 8787 is already in use:UserWarning")
 @pytest.mark.parametrize("T, m", test_data)
 @pytest.mark.parametrize("substitution_location", substitution_locations)
-def test_mstumped_one_subsequence_inf_self_join_first_dimension(
+def test_maamped_one_subsequence_inf_self_join_first_dimension(
     T, m, substitution_location, dask_cluster
 ):
     with Client(dask_cluster) as dask_client:
@@ -35,8 +35,8 @@ def test_mstumped_one_subsequence_inf_self_join_first_dimension(
         T_sub = T.copy()
         T_sub[0, substitution_location] = np.inf
 
-        ref_P, ref_I = naive.mstump(T_sub, m, excl_zone)
-        comp_P, comp_I = mstumped(dask_client, T_sub, m)
+        ref_P, ref_I = naive.maamp(T_sub, m, excl_zone)
+        comp_P, comp_I = maamped(dask_client, T_sub, m)
 
         npt.assert_almost_equal(ref_P, comp_P)
         npt.assert_almost_equal(ref_I, comp_I)
@@ -45,7 +45,7 @@ def test_mstumped_one_subsequence_inf_self_join_first_dimension(
 @pytest.mark.filterwarnings("ignore:\\s+Port 8787 is already in use:UserWarning")
 @pytest.mark.parametrize("T, m", test_data)
 @pytest.mark.parametrize("substitution_location", substitution_locations)
-def test_mstumped_one_subsequence_inf_self_join_all_dimensions(
+def test_maamped_one_subsequence_inf_self_join_all_dimensions(
     T, m, substitution_location, dask_cluster
 ):
     with Client(dask_cluster) as dask_client:
@@ -54,8 +54,8 @@ def test_mstumped_one_subsequence_inf_self_join_all_dimensions(
         T_sub = T.copy()
         T_sub[:, substitution_location] = np.inf
 
-        ref_P, ref_I = naive.mstump(T_sub, m, excl_zone)
-        comp_P, comp_I = mstumped(dask_client, T_sub, m)
+        ref_P, ref_I = naive.maamp(T_sub, m, excl_zone)
+        comp_P, comp_I = maamped(dask_client, T_sub, m)
 
         npt.assert_almost_equal(ref_P, comp_P)
         npt.assert_almost_equal(ref_I, comp_I)
