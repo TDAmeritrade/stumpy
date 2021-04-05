2d433f24091e700b4daafdf439e32bea4440de14
Sean Law <seanmylaw@gmail.com>
2021-04-05 18:23:38 -0400

    Added favicon to root directory

--- docs/conf.py
+++ docs/conf.py
@@ -126,6 +126,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_favicon = 'favicon.ico'
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
