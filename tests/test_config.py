db1c6940e0a67b2bdefd9abb53daf88fde627cce
Sean Law <seanmylaw@gmail.com>
2021-06-16 09:12:43 -0400

    Updated STUMPY_EXCL_ZONE_DENOM test

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,12 +1,11 @@
-import numpy as np
-from stumpy import config
+from stumpy import core, config
 
 
 def test_change_excl_zone_denom():
-    assert config.STUMPY_EXCL_ZONE_DENOM == 4
+    assert core.get_max_window_size(10) == 7
 
-    config.STUMPY_EXCL_ZONE_DENOM = np.inf
-    assert config.STUMPY_EXCL_ZONE_DENOM == np.inf
+    config.STUMPY_EXCL_ZONE_DENOM = 2
+    assert core.get_max_window_size(10) == 6
 
     config.STUMPY_EXCL_ZONE_DENOM = 4
-    assert config.STUMPY_EXCL_ZONE_DENOM == 4
+    assert core.get_max_window_size(10) == 7
