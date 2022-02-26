54ad57df4b20ae1898e86cc330c5b8cc4acb68ab
Sarah <95933072+SaVoAMP@users.noreply.github.com>
2022-02-26 19:47:00 +0100

    Fixed #187 Added Multi-dimensional Motif Discovery
    * [ADD] Add new function for multidimensional motif discovery
    
    * [ADD] Added the first multidimensional motif discovery (prototype)
    
    * [MOD] Changed variable name and added knew nearest neighbor index after setting an exclusion zone
    
    * [ADD] Added converting dataframes to its corresponding array
    
    * [MOD] Changed variable name to don't loose the name of the single dimensions in order to choose the dimensions where the motif is present. WORNING: Lines 75 and 76 only work for data frames currently
    
    * [DEL] Delete all dataframe operations
    
    * [ADD] Add Mmatch function and generalize 1D functions to the multidimensional case
    
    * [MOD] Index shift to first compute the full list and cast to array afterwards
    
    * [MOD] Change function description
    
    * [ADD] Add max-distance and atol parameters and corresponding operations
    
    * [ADD] Checked style guids
    
    * [ADD] Added new cutoff operations
    
    * [ADD] Added min_neighbors parameter
    
    * [ADD] Added possibility to also use only one cutoff value that will be applied to every dimension
    
    * [FIX] Fixed bug (T was getting smaller)
    
    * Ran black again
    
    * deleted blank line after docstring
    
    * [MOD] Changed imports
    
    * [MOD] Precomputed rolling means and standard deviations at the beginning of the function
    
    * [MOD] Rename variables
    
    * [MOD] Updated mmotifs() th have the same ordering as motifs()
    
    * [MOD] Moved cutoff section outside of the while loop
    
    * [MOD] Parameters get passed directly now
    
    * [MOD] Changed some computations, orders and deleted comments
    
    * [FIX] Corrected default cutoff computation
    
    * [MOD] The returns 'motif_subspaces' and 'motif_mdls' are lists instead of arrays now
    
    * [MOD] Changed positioning of returns and deleted parantheses
    
    * [MOD] Changed positioning of returns so that all 'appends' are at the same place
    
    * [ADD] Add new function for non-normalized multidimensional motif discovery
    
    * [ADD] aamp_mmotifs import and uncomment normalize and p parameters
    
    * [ADD] Added possibility of doing guided search since the user is able to choose the motif's dimension himself
    
    * [ADD] Added possibility of doing constrained searchsince the user can explicitly include specific dimensions
    
    * [ADD] Added discord discovery
    
    * [DEL] Removed discord discovery
    
    * [MOD] Always return 'mdls'
    
    * [MOD] Changed the way of accessing k
    
    * [MOD] Changed description for the parameter 'k' and switched order of appends
    
    * [ADD] Added 'k' and 'include' parameters

---
+++ stumpy/mmotifs.py
@@ -0,0 +1,198 @@
+import numpy as np
+import logging
+
+from .aamp_mmotifs import aamp_mmotifs
+from . import core, config, mdl, match
+
+logger = logging.getLogger(__name__)
+
+
+@core.non_normalized(aamp_mmotifs)
+def mmotifs(
+    T,
+    P,
+    I,
+    min_neighbors=1,
+    max_distance=None,
+    cutoffs=None,
+    max_matches=10,
+    max_motifs=1,
+    atol=1e-8,
+    k=None,
+    include=None,
+    normalize=True,
+    p=2.0,
+):
+    """
+    Discover the top motifs for the multi-dimensional time series `T`
+
+    Parameters
+    ----------
+    T: numpy.ndarray
+        The multi-dimensional time series or sequence
+
+    P: numpy.ndarray
+        Multi-dimensional Matrix Profile of T
+
+    I: numpy.ndarray
+        Multi-dimensional Matrix Profile indices
+
+    min_neighbors : int, default 1
+        The minimum number of similar matches a subsequence needs to have in order
+        to be considered a motif. This defaults to `1`, which means that a subsequence
+        must have at least one similar match in order to be considered a motif.
+
+    max_distance: flaot, default None
+        Maximal distance that is allowed between a query subsequence
+        (a candidate motif) and all subsequences in T to be considered as a match.
+        If None, this defaults to
+        `np.nanmax([np.nanmean(D) - 2 * np.nanstd(D), np.nanmin(D)])`
+        (i.e. at least the closest match will be returned).
+
+    cutoffs: numpy.ndarray or float, default None
+        The largest matrix profile value (distance) for each dimension of the
+        multidimensional matrix profile that a multidimenisonal candidate motif is
+        allowed to have.
+        If cutoffs is only one value, these value will be applied to every dimension.
+
+    max_matches: int, default 10
+        The maximum amount of similar matches (nearest neighbors) of a motif
+        representative to be returned.
+        The first match is always the self-match for each motif.
+
+    max_motifs: int, default 1
+        The maximum number of motifs to return
+
+    atol : float, default 1e-8
+        The absolute tolerance parameter. This value will be added to `max_distance`
+        when comparing distances between subsequences.
+
+    k: int, default None
+        The number of dimensions (k + 1) in which a motif is present.
+        This value is available for doing guided search or - together with 'include' -
+        for constrained search.
+        The value will be applied to the discovery of all motifs.
+        If k is None, the value will automatically be computed for each motif using
+        MDL (unconstrained search).
+        For more informatioin on search types, see DOI: 10.1109/ICDM.2017.66s
+
+    include: numpy.ndarray, default None
+        A list of (zero based) indices corresponding to the dimensions in T that must be
+        included in the constrained multidimensional motif search. For more information,
+        see Section IV D in: DOI: 10.1109/ICDM.2017.66
+
+    normalize : bool, default True
+        When set to `True`, this z-normalizes subsequences prior to computing distances.
+        Otherwise, this function gets re-routed to its complementary non-normalized
+        equivalent set in the `@core.non_normalized` function decorator.
+
+    p : float, default 2.0
+        The p-norm to apply for computing the Minkowski distance. This parameter is
+        ignored when `normalize == False`.
+
+    Returns
+    -------
+    motif_distances: numpy.ndarray
+        The distances corresponding to a set of subsequence matches for each motif.
+
+    motif_indices: numpy.ndarray
+        The indices corresponding to a set of subsequences matches for each motif.
+
+    motif_subspaces: list
+        A list consisting of arrays that contain the `k`-dimensional
+        subspace for each motif.
+
+    motif_mdls: list
+        A list consisting of arrays that contain the mdl results for
+        finding the dimension of each motif
+    """
+    T = core._preprocess(T)
+    m = T.shape[-1] - P.shape[-1] + 1
+    reset_k = False
+
+    if max_motifs < 1:  # pragma: no cover
+        logger.warning(
+            "The maximum number of motifs, `max_motifs`, "
+            "must be greater than or equal to 1"
+        )
+        logger.warning("`max_motifs` has been set to `1`")
+        max_motifs = 1
+
+    T, M_T, Σ_T = core.preprocess(T, m)
+
+    excl_zone = int(np.ceil(m / config.STUMPY_EXCL_ZONE_DENOM))
+
+    if max_matches is None:
+        max_matches = np.inf
+
+    if cutoffs is None:
+        P_copy = P.copy().astype(np.float64)
+        P_copy[np.isinf(P_copy)] = np.nan
+        cutoffs = np.nanmax(
+            [
+                np.nanmean(P_copy, axis=1) - 2.0 * np.nanstd(P_copy, axis=1),
+                np.nanmin(P_copy, axis=1),
+            ],
+            axis=0,
+        )
+    if np.isscalar(cutoffs):
+        cutoffs = np.full(P.shape[0], cutoffs)
+
+    motif_distances = []
+    motif_indices = []
+    motif_subspaces = []
+    motif_mdls = []
+
+    candidate_idx = np.argmin(P, axis=1)
+    nn_idx = I[np.arange(len(candidate_idx)), candidate_idx]
+
+    while len(motif_distances) < max_motifs:
+        mdls, subspaces = mdl(T, m, candidate_idx, nn_idx, include)
+        if k is None:
+            k = np.argmin(mdls)
+            reset_k = True
+        subspace_k = subspaces[k]
+
+        motif_idx = candidate_idx[k]
+        motif_value = P[k, motif_idx]
+
+        if (
+            motif_value > cutoffs[k]
+            or not np.isfinite(motif_value)
+            or (isinstance(max_distance, float) and motif_value > max_distance)
+        ):
+            break
+
+        query_matches = match(
+            Q=T[subspace_k, motif_idx : motif_idx + m],
+            T=T[subspace_k],
+            M_T=M_T[subspace_k],
+            Σ_T=Σ_T[subspace_k],
+            max_matches=max_matches,
+            max_distance=max_distance,
+            atol=atol,
+            normalize=normalize,
+            p=p,
+        )
+
+        if len(query_matches) > min_neighbors:
+            motif_distances.append(query_matches[:, 0])
+            motif_indices.append(query_matches[:, 1])
+            motif_subspaces.append(subspace_k)
+            motif_mdls.append(mdls)
+
+        for idx in query_matches[:, 1]:
+            core.apply_exclusion_zone(P, idx, excl_zone, np.inf)
+        candidate_idx = np.argmin(P, axis=1)
+        nn_idx = I[np.arange(len(candidate_idx)), candidate_idx]
+        if reset_k:
+            k = None
+
+    motif_distances = core._jagged_list_to_array(
+        motif_distances, fill_value=np.nan, dtype=np.float64
+    )
+    motif_indices = core._jagged_list_to_array(
+        motif_indices, fill_value=-1, dtype=np.int64
+    )
+
+    return motif_distances, motif_indices, motif_subspaces, motif_mdls
