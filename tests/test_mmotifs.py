5518e681dde48e345cb2f1d45b6ecd1ae4a2e40a
SaVoAMP <sarah.volkamer@gmail.com>
2022-02-28 13:55:41 +0100

    [ADD] Added match functions from 'test_motifs.py'

--- tests/test_mmotifs.py
+++ tests/test_mmotifs.py
@@ -1,11 +1,50 @@
 import numpy as np
 import numpy.testing as npt
+import pytest
 
 from stumpy.mmotifs import mmotifs
 from stumpy.mstump import mstump
+from stumpy import match
+
+import naive
 
 
 # These are tests for multidimensional motif discovery
+
+
+def naive_match(Q, T, excl_zone, max_distance):
+    m = Q.shape[0]
+    D = naive.distance_profile(Q, T, m)
+
+    matches = []
+    for i in range(D.size):
+        dist = D[i]
+        if dist <= max_distance:
+            matches.append(i)
+
+    # Removes indices that are inside the exclusion zone of some occurrence with
+    # a smaller distance to the query
+    matches.sort(key=lambda x: D[x])
+    result = []
+    while len(matches) > 0:
+        o = matches[0]
+        result.append([D[o], o])
+        matches = [x for x in matches if x < o - excl_zone or x > o + excl_zone]
+
+    return np.array(result, dtype=object)
+
+
+test_data = [
+    (
+        np.array([0.0, 1.0, 0.0]),
+        np.array([0.0, 1.0, 0.0, -1.0, -1.0, 0.0, 1.0, 0.0, -0.5]),
+    ),
+    (
+        np.array([0.0, 1.0, 2.0]),
+        np.array([0.1, 1.0, 2.0, 3.0, -1.0, 0.1, 1.0, 2.0, -0.5]),
+    ),
+    (np.random.uniform(-1000, 1000, [8]), np.random.uniform(-1000, 1000, [64])),
+]
 
 
 def test_motifs_multidimensional_one_motif_all_dimensions():
@@ -73,3 +112,26 @@ def test_motifs_multidimensional_one_motif_all_dimensions():
 
 
 # def test_motifs_multidimensional_two_motifs_all_dimensions():
+
+
+@pytest.mark.parametrize("Q, T", test_data)
+def test_match(Q, T):
+    m = Q.shape[0]
+    excl_zone = int(np.ceil(m / 4))
+    max_distance = 0.3
+
+    left = naive_match(
+        Q,
+        T,
+        excl_zone,
+        max_distance=max_distance,
+    )
+
+    right = match(
+        Q,
+        T,
+        max_matches=None,
+        max_distance=lambda D: max_distance,  # also test lambda functionality
+    )
+
+    npt.assert_almost_equal(left, right)
