46ae307040424bd30f1eed40b2723e5c526d7350
SaVoAMP <sarah.volkamer@gmail.com>
2022-03-02 16:38:55 +0100

    [ADD] Added function to test 2-dimensional motif discovery

--- tests/test_aamp_mmotifs.py
+++ tests/test_aamp_mmotifs.py
@@ -1,9 +1,74 @@
-# Dummy test
+import numpy as np
+import numpy.testing as npt
+
+from stumpy.mmotifs import mmotifs
+from stumpy.mstump import mstump
 
 
-def increment(x):
-    return x + 1
+# These are tests for non-normalized multidimensional motif discovery
 
 
-def test_answer():
-    assert increment(3) == 4
+def test_motifs_multidimensional_one_motif_all_dimensions():
+    # Find the two dimensional motif pair
+
+    # Arrange
+    motif_distances_expected = np.array([[0.        , 1.72474487]])
+    motif_indices_expected = np.array([[0, 5]])
+    motif_subspaces_expected = [np.array([1, 2])]
+    motif_mdls_expected = [np.array([187.        , 188.        , 191.26466251, 196.        ])]
+
+    # Act
+    T = np.array(
+        [
+            [5.0, 0.0, 3.0, 3.0, 7.0, 9.0, 3.0, 5.0, 2.0, 4.0, 7.0, 6.0, 8.0, 8.0, 1.0],
+            [
+                7.0,
+                3.0,
+                5.0,
+                9.0,
+                8.0,
+                7.0,
+                4.0,
+                5.0,
+                10.0,
+                8.0,
+                4.0,
+                3.0,
+                2.0,
+                0.0,
+                1.0,
+            ],
+            [6.0, 7.0, 7.0, 8.0, 1.0, 5.0, 9.0, 8.0, 9.0, 4.0, 3.0, 0.0, 3.0, 5.0, 0.0],
+            [
+                0.0,
+                1.0,
+                3.0,
+                2.0,
+                6.0,
+                1.0,
+                9.0,
+                10.0,
+                1.0,
+                2.0,
+                2.0,
+                5.0,
+                1.0,
+                0.0,
+                4.0,
+            ],
+        ]
+    )
+    m = 3
+
+    P, I = mstump(T, m, normalize=False)
+    motif_distances, motif_indices, motif_subspaces, motif_mdls = mmotifs(
+        T, P, I, max_distance=np.inf, max_matches=2, k=1, normalize=False
+    )
+
+    # Assert
+    npt.assert_array_almost_equal(motif_distances_expected, motif_distances)
+    npt.assert_array_almost_equal(motif_indices_expected, motif_indices)
+    npt.assert_array_almost_equal(motif_subspaces_expected, motif_subspaces)
+    npt.assert_array_almost_equal(motif_mdls_expected, motif_mdls)
+
+
