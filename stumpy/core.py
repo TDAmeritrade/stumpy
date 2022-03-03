244144a4a92c621145c589aef01701d9c8465667
Sean Law <seanmylaw@gmail.com>
2022-03-02 19:38:33 -0500

    Minor test

--- stumpy/core.py
+++ stumpy/core.py
@@ -1081,8 +1081,8 @@ def _p_norm_distance_profile(Q, T, p=2.0, n_threads=1):
     else:
         for i in prange(k):
             tmp = Q - T[i : i + m]
-            tmp2 = np.abs(tmp2)
-            tmp3 = np.power(tmp3, p)
+            tmp2 = np.abs(tmp)
+            tmp3 = np.power(tmp2, p)
             p_norm_profile[i] = np.sum(tmp3)
             # p_norm_profile[i] = np.sum(np.power(np.abs(Q - T[i : i + m]), p))
 
