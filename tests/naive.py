448f456db02708a4b8eaf999737bd6d12137c285
ninimama <nimasarajpoor@gmail.com>
2022-05-05 13:24:22 -0600

    Add parameter to choose traversal method

--- tests/naive.py
+++ tests/naive.py
@@ -156,7 +156,7 @@ def stamp(T_A, m, T_B=None, exclusion_zone=None):
     return result
 
 
-def stump(T_A, m, T_B=None, exclusion_zone=None):
+def stump(T_A, m, T_B=None, exclusion_zone=None, row_traversal=False):
     """
     Traverse distance matrix along the diagonals and update the matrix profile and
     matrix profile indices
@@ -181,47 +181,65 @@ def stump(T_A, m, T_B=None, exclusion_zone=None):
     if exclusion_zone is None:
         exclusion_zone = int(np.ceil(m / config.STUMPY_EXCL_ZONE_DENOM))
 
-    if ignore_trivial:
-        diags = np.arange(exclusion_zone + 1, n_A - m + 1)
-    else:
-        diags = np.arange(-(n_A - m + 1) + 1, n_B - m + 1)
 
-    P = np.full((l, 3), np.inf)
-    I = np.full((l, 3), -1, dtype=np.int64)
-
-    for k in diags:
-        if k >= 0:
-            iter_range = range(0, min(n_A - m + 1, n_B - m + 1 - k))
+    if row_traversal:
+        if ignore_trivial:  # self-join
+            result = np.array(
+                [
+                    mass_PI(Q, T_A, m, i, exclusion_zone, True)
+                    for i, Q in enumerate(core.rolling_window(T_A, m))
+                ],
+                dtype=object,
+            )
         else:
-            iter_range = range(-k, min(n_A - m + 1, n_B - m + 1 - k))
+            result = np.array(
+                [mass_PI(Q, T_B, m) for Q in core.rolling_window(T_A, m)],
+                dtype=object,
+            )
+        return result
 
-        for i in iter_range:
-            D = distance_matrix[i, i + k]
-            if D < P[i, 0]:
-                P[i, 0] = D
-                I[i, 0] = i + k
+    else: #diagonal traversal
+        if ignore_trivial:
+            diags = np.arange(exclusion_zone + 1, n_A - m + 1)
+        else:
+            diags = np.arange(-(n_A - m + 1) + 1, n_B - m + 1)
 
-            if ignore_trivial:  # Self-joins only
-                if D < P[i + k, 0]:
-                    P[i + k, 0] = D
-                    I[i + k, 0] = i
+        P = np.full((l, 3), np.inf)
+        I = np.full((l, 3), -1, dtype=np.int64)
 
-                if i < i + k:
-                    # Left matrix profile and left matrix profile index
-                    if D < P[i + k, 1]:
-                        P[i + k, 1] = D
-                        I[i + k, 1] = i
+        for k in diags:
+            if k >= 0:
+                iter_range = range(0, min(n_A - m + 1, n_B - m + 1 - k))
+            else:
+                iter_range = range(-k, min(n_A - m + 1, n_B - m + 1 - k))
 
-                    if D < P[i, 2]:
-                        # right matrix profile and right matrix profile index
-                        P[i, 2] = D
-                        I[i, 2] = i + k
+            for i in iter_range:
+                D = distance_matrix[i, i + k]
+                if D < P[i, 0]:
+                    P[i, 0] = D
+                    I[i, 0] = i + k
 
-    result = np.empty((l, 4), dtype=object)
-    result[:, 0] = P[:, 0]
-    result[:, 1:4] = I[:, :]
+                if ignore_trivial:  # Self-joins only
+                    if D < P[i + k, 0]:
+                        P[i + k, 0] = D
+                        I[i + k, 0] = i
 
-    return result
+                    if i < i + k:
+                        # Left matrix profile and left matrix profile index
+                        if D < P[i + k, 1]:
+                            P[i + k, 1] = D
+                            I[i + k, 1] = i
+
+                        if D < P[i, 2]:
+                            # right matrix profile and right matrix profile index
+                            P[i, 2] = D
+                            I[i, 2] = i + k
+
+        result = np.empty((l, 4), dtype=object)
+        result[:, 0] = P[:, 0]
+        result[:, 1:4] = I[:, :]
+
+        return result
 
 
 def aamp(T_A, m, T_B=None, exclusion_zone=None, p=2.0):
