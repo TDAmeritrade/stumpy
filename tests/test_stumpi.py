c5d23452b08e76227a050dfc0cdf5632472706b8
ninimama <nimasarajpoor@gmail.com>
2022-06-25 23:06:20 -0600

    Fix shape of output for KNN test

--- tests/test_stumpi.py
+++ tests/test_stumpi.py
@@ -913,9 +913,9 @@ def test_stumpi_self_join_KNN():
         comp_left_I = stream.left_I_
 
         ref_mp = naive.stump(stream.T_, m, exclusion_zone=zone, row_wise=True, k=k)
-        ref_P = ref_mp[:, 0].reshape(-1, 1)
-        ref_I = ref_mp[:, 1].reshape(-1, 1)
-        ref_left_I = ref_mp[:, 2].astype(np.int64)
+        ref_P = ref_mp[:, :k]
+        ref_I = ref_mp[:, k : 2 * k]
+        ref_left_I = ref_mp[:, 2 * k].astype(np.int64)
         ref_left_P = np.full_like(ref_left_I, np.inf, dtype=np.float64)
         for i, j in enumerate(ref_left_I):
             if j >= 0:
