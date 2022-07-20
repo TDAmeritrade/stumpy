aaa8ff7bbe07bf4205e1d04476e11c95d01d038b
ninimama <nimasarajpoor@gmail.com>
2022-07-20 13:39:55 -0600

    add new test function

--- tests/test_scrump.py
+++ tests/test_scrump.py
@@ -854,3 +854,22 @@ def test_prescrump_self_join_larger_window_m_5_k_5(T_A, T_B):
 
             npt.assert_almost_equal(ref_P, comp_P)
             npt.assert_almost_equal(ref_I, comp_I)
+
+
+@pytest.mark.parametrize("T_A, T_B", test_data)
+def test_prescrump_A_B_join_larger_window_m_5_k_5(T_A, T_B):
+    m = 5
+    k = 5
+    zone = int(np.ceil(m / 4))
+    if len(T_A) > m and len(T_B) > m:
+        for s in range(1, zone + 1):
+            seed = np.random.randint(100000)
+
+            np.random.seed(seed)
+            ref_P, ref_I = naive.prescrump(T_A, m, T_B, s=s, k=k)
+
+            np.random.seed(seed)
+            comp_P, comp_I = prescrump(T_A, m, T_B, s=s, k=k)
+
+            npt.assert_almost_equal(ref_P, comp_P)
+            npt.assert_almost_equal(ref_I, comp_I)
