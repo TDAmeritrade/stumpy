d1469aaaab01014903244f0efa20933b9765a640
Sean Law <seanmylaw@gmail.com>
2022-08-28 16:51:29 -0400

    Fixed black formatting

--- docs/conf.py
+++ docs/conf.py
@@ -111,9 +111,7 @@ html_theme_options = {
     ],
 }
 
-html_context = {
-   "default_mode": "light"
-}
+html_context = {"default_mode": "light"}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
