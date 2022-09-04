ee95e705b2a39a8c9ab0ae7775cdb80a9c128268
SolidAhmad <nimasarajpoor@gmail.com>
2022-09-04 12:15:57 -0600

    use rolling min and max to find constant sequences

--- stumpy/core.py
+++ stumpy/core.py
@@ -1756,9 +1756,10 @@ def preprocess_diagonal(T, m):
     T, T_subseq_isfinite = preprocess_non_normalized(T, m)
     M_T, Σ_T = compute_mean_std(T, m)
     T_sliding_min = _rolling_nanmin_1d(T, m)
-    T_subseq_isconstant = M_T == T_sliding_min
-    Σ_T[T_subseq_isconstant] = 0.0
-    Σ_T[Σ_T <= 0] = 1.0
+    T_sliding_max = _rolling_nanmax_1d(T, m)
+    T_subseq_isconstant = T_sliding_min == T_sliding_max
+    Σ_T[T_subseq_isfinite & T_subseq_isconstant] = 0.0
+    Σ_T[Σ_T <= config.STUMPY_STDDEV_THRESHOLD] = 1.0
     Σ_T_inverse = 1.0 / Σ_T
     M_T_m_1, _ = compute_mean_std(T, m - 1)
 
