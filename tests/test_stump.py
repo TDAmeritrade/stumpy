9ceaf11d56a2355eb2ff2683e7fb260d5cf3d442
SolidAhmad <nimasarajpoor@gmail.com>
2022-09-06 18:38:39 -0600

    Add new test function

--- tests/test_stump.py
+++ tests/test_stump.py
@@ -299,3 +299,31 @@ def test_stump_identical_subsequence_self_join_rare_cases_2():
         npt.assert_almost_equal(
             ref_mp[:, 0], comp_mp[:, 0], decimal=config.STUMPY_TEST_PRECISION
         )  # ignore indices
+
+
+def test_stump_identical_subsequence_self_join_rare_cases_3():
+    m = 3
+    zone = int(np.ceil(m / 4))
+
+    seed_values = [27343, 84451]
+    for seed in seed_values:
+        np.random.seed(seed)
+
+        identical = np.random.rand(8)
+        T_A = np.random.rand(20)
+        T_A[1 : 1 + identical.shape[0]] = identical * 0.00001
+        T_A[11 : 11 + identical.shape[0]] = identical * 100000
+
+        ref_mp = naive.stump(T_A, m, exclusion_zone=zone, row_wise=True)
+        comp_mp = stump(T_A, m, ignore_trivial=True)
+        naive.replace_inf(ref_mp)
+        naive.replace_inf(comp_mp)
+        npt.assert_almost_equal(
+            ref_mp[:, 0], comp_mp[:, 0], decimal=config.STUMPY_TEST_PRECISION
+        )  # ignore indices
+
+        comp_mp = stump(pd.Series(T_A), m, ignore_trivial=True)
+        naive.replace_inf(comp_mp)
+        npt.assert_almost_equal(
+            ref_mp[:, 0], comp_mp[:, 0], decimal=config.STUMPY_TEST_PRECISION
+        )  # ignore indices
