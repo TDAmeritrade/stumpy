d3de095ce15dadefce0a87c79de9a445035f32b5
ninimama <nimasarajpoor@gmail.com>
2022-09-16 14:36:24 -0600

    Add test function with fixed seed

--- tests/test_snippets.py
+++ tests/test_snippets.py
@@ -240,3 +240,44 @@ def test_snippet_low_percentage():
     )
     npt.assert_almost_equal(ref_areas, cmp_areas, decimal=config.STUMPY_TEST_PRECISION)
     npt.assert_almost_equal(ref_regimes, cmp_regimes)
+
+
+def test_snippet_fixed_seeds():
+    seed = 15
+    np.random.seed(seed)
+    T = np.random.uniform(-1000, 1000, [64]).astype(np.float64)
+    m = 8
+    k = 3
+    s = 3
+
+    (
+        ref_snippets,
+        ref_indices,
+        ref_profiles,
+        ref_fractions,
+        ref_areas,
+        ref_regimes,
+    ) = naive.mpdist_snippets(T, m, k, s=s)
+    (
+        cmp_snippets,
+        cmp_indices,
+        cmp_profiles,
+        cmp_fractions,
+        cmp_areas,
+        cmp_regimes,
+    ) = snippets(T, m, k, s=s)
+
+    npt.assert_almost_equal(
+        ref_snippets, cmp_snippets, decimal=config.STUMPY_TEST_PRECISION
+    )
+    npt.assert_almost_equal(
+        ref_indices, cmp_indices, decimal=config.STUMPY_TEST_PRECISION
+    )
+    npt.assert_almost_equal(
+        ref_profiles, cmp_profiles, decimal=config.STUMPY_TEST_PRECISION
+    )
+    npt.assert_almost_equal(
+        ref_fractions, cmp_fractions, decimal=config.STUMPY_TEST_PRECISION
+    )
+    npt.assert_almost_equal(ref_areas, cmp_areas, decimal=config.STUMPY_TEST_PRECISION)
+    npt.assert_almost_equal(ref_regimes, cmp_regimes)
