0c80852759d05965a25989810ae8b9e032955af1
SolidAhmad <nimasarajpoor@gmail.com>
2022-10-12 23:52:44 -0600

    minor changes

--- tests/naive.py
+++ tests/naive.py
@@ -1828,9 +1828,10 @@ def _total_diagonal_ndists(tile_lower_diag, tile_upper_diag, tile_height, tile_w
 
 def merge_topk_PI(PA, PB, IA, IB):
     if PA.ndim == 1:
-        mask = (PB < PA) & (IB != IA)
-        PA[mask] = PB[mask]
-        IA[mask] = IB[mask]
+        for i in range(PA.shape[0]):
+            if PB[i] < PA[i] and IB[i] != IA[i]:
+                PA[i] = PB[i]
+                IA[i] = IB[i]
         return
 
     k = PA.shape[1]
@@ -1870,9 +1871,10 @@ def merge_topk_ρI(ρA, ρB, IA, IB):
     # ties: [0_B, 0_A, 0'_A, 1_B, 1'_B, 1_A], and we just need to keep the second
     # half of this array, and discard the first half.
     if ρA.ndim == 1:
-        mask = (ρB > ρA) & (IB != IA)
-        ρA[mask] = ρB[mask]
-        IA[mask] = IB[mask]
+        for i in range(ρA.shape[0]):
+            if ρB[i] > ρA[i] and IB[i] != IA[i]:
+                ρA[i] = ρB[i]
+                IA[i] = IB[i]
         return
 
     k = ρA.shape[1]
