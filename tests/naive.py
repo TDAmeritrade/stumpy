f7c65a92acddc707c54389bf6fabfa2916afacd1
ninimama <nimasarajpoor@gmail.com>
2022-11-10 14:24:49 -0700

    Add support for top-k to naive aamp

--- tests/naive.py
+++ tests/naive.py
@@ -267,17 +267,22 @@ def stump(T_A, m, T_B=None, exclusion_zone=None, row_wise=False, k=1):
     return result
 
 
-def aamp(T_A, m, T_B=None, exclusion_zone=None, p=2.0):
+def aamp(T_A, m, T_B=None, exclusion_zone=None, p=2.0, row_wise=False, k=1):
+    """
+    Traverse distance matrix diagonally and update the top-k matrix profile and
+    matrix profile indices if the parameter `row_wise` is set to `False`. If the
+    parameter `row_wise` is set to `True`, it is a row-wise traversal.
+    """
     T_A = np.asarray(T_A)
     T_A = T_A.copy()
 
     if T_B is None:
-        T_B = T_A.copy()
         ignore_trivial = True
+        T_B = T_A.copy()
     else:
+        ignore_trivial = False
         T_B = np.asarray(T_B)
         T_B = T_B.copy()
-        ignore_trivial = False
 
     T_A[np.isinf(T_A)] = np.nan
     T_B[np.isinf(T_B)] = np.nan
@@ -285,53 +290,84 @@ def aamp(T_A, m, T_B=None, exclusion_zone=None, p=2.0):
     rolling_T_A = core.rolling_window(T_A, m)
     rolling_T_B = core.rolling_window(T_B, m)
 
+    distance_matrix = cdist(rolling_T_A, rolling_T_B, metric="minkowski", p=p)
+
     n_A = T_A.shape[0]
     n_B = T_B.shape[0]
     l = n_A - m + 1
     if exclusion_zone is None:
         exclusion_zone = int(np.ceil(m / config.STUMPY_EXCL_ZONE_DENOM))
 
-    distance_matrix = cdist(rolling_T_A, rolling_T_B, metric="minkowski", p=p)
+    P = np.full((l, k + 2), np.inf, dtype=np.float64)
+    I = np.full((l, k + 2), -1, dtype=np.int64)  # two more columns are to store
+    # ... left and right top-1 matrix profile indices
 
-    if ignore_trivial:
-        diags = np.arange(exclusion_zone + 1, n_A - m + 1)
+    if row_wise:
+        if ignore_trivial:  # self-join
+            for i in range(l):
+                apply_exclusion_zone(distance_matrix[i], i, exclusion_zone, np.inf)
+
+        for i, D in enumerate(distance_matrix):  # D: distance profile
+            # self-join / AB-join: matrix proifle and indices
+            indices = np.argsort(D)[:k]
+            P[i, :k] = D[indices]
+            indices[P[i, :k] == np.inf] = -1
+            I[i, :k] = indices
+
+            # self-join: left matrix profile index (top-1)
+            if ignore_trivial and i > 0:
+                IL = np.argmin(D[:i])
+                if D[IL] == np.inf:
+                    IL = -1
+                I[i, k] = IL
+
+            # self-join: right matrix profile index (top-1)
+            if ignore_trivial and i < D.shape[0]:
+                IR = i + np.argmin(D[i:])  # offset by `i` to get true index
+                if D[IR] == np.inf:
+                    IR = -1
+                I[i, k + 1] = IR
+
     else:
-        diags = np.arange(-(n_A - m + 1) + 1, n_B - m + 1)
-
-    P = np.full((l, 3), np.inf)
-    I = np.full((l, 3), -1, dtype=np.int64)
-
-    for k in diags:
-        if k >= 0:
-            iter_range = range(0, min(n_A - m + 1, n_B - m + 1 - k))
+        if ignore_trivial:
+            diags = np.arange(exclusion_zone + 1, n_A - m + 1)
         else:
-            iter_range = range(-k, min(n_A - m + 1, n_B - m + 1 - k))
+            diags = np.arange(-(n_A - m + 1) + 1, n_B - m + 1)
 
-        for i in iter_range:
-            D = distance_matrix[i, i + k]
-            if D < P[i, 0]:
-                P[i, 0] = D
-                I[i, 0] = i + k
+        for g in diags:
+            if g >= 0:
+                iter_range = range(0, min(n_A - m + 1, n_B - m + 1 - g))
+            else:
+                iter_range = range(-g, min(n_A - m + 1, n_B - m + 1 - g))
 
-            if ignore_trivial:  # Self-joins only
-                if D < P[i + k, 0]:
-                    P[i + k, 0] = D
-                    I[i + k, 0] = i
+            for i in iter_range:
+                d = distance_matrix[i, i + g]
+                if d < P[i, k - 1]:
+                    idx = searchsorted_right(P[i], d)
+                    # to keep the top-k, we must discard the last element.
+                    P[i, :k] = np.insert(P[i, :k], idx, d)[:-1]
+                    I[i, :k] = np.insert(I[i, :k], idx, i + g)[:-1]
 
-                if i < i + k:
-                    # Left matrix profile and left matrix profile index
-                    if D < P[i + k, 1]:
-                        P[i + k, 1] = D
-                        I[i + k, 1] = i
+                if ignore_trivial:  # Self-joins only
+                    if d < P[i + g, k - 1]:
+                        idx = searchsorted_right(P[i + g], d)
+                        P[i + g, :k] = np.insert(P[i + g, :k], idx, d)[:-1]
+                        I[i + g, :k] = np.insert(I[i + g, :k], idx, i)[:-1]
 
-                    if D < P[i, 2]:
-                        # right matrix profile and right matrix profile index
-                        P[i, 2] = D
-                        I[i, 2] = i + k
+                    if i < i + g:
+                        # Left matrix profile and left matrix profile index
+                        if d < P[i + g, k]:
+                            P[i + g, k] = d
+                            I[i + g, k] = i
 
-    result = np.empty((l, 4), dtype=object)
-    result[:, 0] = P[:, 0]
-    result[:, 1:4] = I[:, :]
+                        if d < P[i, k + 1]:
+                            # right matrix profile and right matrix profile index
+                            P[i, k + 1] = d
+                            I[i, k + 1] = i + g
+
+    result = np.empty((l, 2 * k + 2), dtype=object)
+    result[:, :k] = P[:, :k]
+    result[:, k:] = I[:, :]
 
     return result
 
