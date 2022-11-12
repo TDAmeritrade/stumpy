97d7bd623d386281c705d340be07d5c94231b583
Sean Law <seanmylaw@gmail.com>
2022-11-12 11:40:45 -0500

    Added "--editable" install mode to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+mode=""
+
 echo "y" |  python -m pip uninstall stumpy
-python -m pip install .
+
+# Parse command line arguments
+for var in "$@"
+do
+    if [[ $var == "dev" ]]; then
+        echo 'Installing stumpy locally in "--editable" mode'
+        mode="--editable"
+    elif [[ $var == "edit" ]]; then
+        echo 'Installing stumpy locally in "--editable" mode'
+        mode="--editable"
+    elif [[ $var == "-e" ]]; then
+        echo 'Installing stumpy locally in "--editable" mode'
+        mode="--editable"
+    else
+        echo "Installing stumpy in site-packages"
+    fi
+done
+
+python -m pip install $mode .
 rm -rf stumpy.egg-info build dist __pycache__
