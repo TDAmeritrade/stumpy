e174d3c9265128c46063f54ac9d75b60c4d7cc83
SolidAhmad <nimasarajpoor@gmail.com>
2022-12-28 06:33:48 -0700

    Prevent computed pearson from exceeding 1.0

--- stumpy/stump.py
+++ stumpy/stump.py
@@ -207,6 +207,8 @@ def _compute_diagonal(
                     pearson = 0.5
                 else:
                     pearson = cov * Σ_T_inverse[uint64_j] * σ_Q_inverse[uint64_i]
+                    if pearson > 1.0:
+                        pearson = 1.0
 
                 # `ρ[thread_idx, i, :]` is sorted ascendingly and MUST be updated
                 # when the newly-calculated `pearson` value becomes greater than the
