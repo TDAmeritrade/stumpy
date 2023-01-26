926cf2a3960d151c654a4a9b64bacd58ae405618
Sean Law <seanmylaw@gmail.com>
2023-01-26 17:16:29 -0500

    Added header to file

--- stumpy/cache.py
+++ stumpy/cache.py
@@ -1,3 +1,7 @@
+# STUMPY
+# Copyright 2019 TD Ameritrade. Released under the terms of the 3-Clause BSD license.
+# STUMPY is a trademark of TD Ameritrade IP Company, Inc. All rights reserved.
+
 import ast
 import pkgutil
 import pathlib
