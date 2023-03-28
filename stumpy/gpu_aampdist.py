9e8b55aaf25c6df0278c1e1e333c068969ccb46e
nimasarajpoor <nimasarajpoor@gmail.com>
2023-03-28 00:20:21 -0400

    fix flake8 format

--- stumpy/gpu_aampdist.py
+++ stumpy/gpu_aampdist.py
@@ -2,8 +2,6 @@
 # Copyright 2019 TD Ameritrade. Released under the terms of the 3-Clause BSD license.
 # STUMPY is a trademark of TD Ameritrade IP Company, Inc. All rights reserved.
 
-import functools
-
 from . import gpu_aamp
 from .core import _aampdist
 
