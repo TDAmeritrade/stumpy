549c8a9f005db30f396513deaaf5197783d8e45b
nimasarajpoor <nimasarajpoor@gmail.com>
2023-08-05 10:04:25 -0400

    increase decimal coverage in assertion

--- tests/test_precision.py
+++ tests/test_precision.py
@@ -101,7 +101,7 @@ def test_calculate_squared_distance():
             comp = dist_ij - dist_ji
             ref = 0.0
 
-            npt.assert_almost_equal(ref, comp)
+            npt.assert_almost_equal(ref, comp, decimal=15)
 
 
 def test_snippets():
