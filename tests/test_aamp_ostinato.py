79ae5ec82d17590588bf6cf829cf62f70965d3a6
NimaSarajpoor <nimasarajpoor@gmail.com>
2024-06-15 07:16:33 -0400

    Add test to expose issue

--- tests/test_aamp_ostinato.py
+++ tests/test_aamp_ostinato.py
@@ -80,3 +80,60 @@ def test_deterministic_ostinatoed(seed, dask_cluster):
             npt.assert_almost_equal(ref_radius, comp_radius)
             npt.assert_almost_equal(ref_Ts_idx, comp_Ts_idx)
             npt.assert_almost_equal(ref_subseq_idx, comp_subseq_idx)
+
+
+def test_input_not_overwritten():
+    # ostinato preprocesses its input, a list of time series,
+    # by replacing nan value with 0 in each time series.
+    # This test ensures that the original input is not overwritten
+    m = 50
+    Ts = [np.random.rand(n) for n in [64, 128, 256]]
+
+    # insert at least one nan value
+    nan_size = [np.random.choice(np.arange(1, len(T) + 1)) for T in Ts]
+    for i in range(len(Ts)):
+        IDX = np.random.choice(np.arange(len(Ts[i])), size=nan_size[i], replace=False)
+        Ts[i][IDX] = np.nan
+
+    # raise error if ostinato overwrite its input
+    Ts_input = [T.copy() for T in Ts]
+    stumpy.aamp_ostinato(Ts_input, m)
+    for i in range(len(Ts)):
+        T_ref = Ts[i]
+        T_comp = Ts_input[i]
+        npt.assert_almost_equal(T_ref[np.isfinite(T_ref)], T_comp[np.isfinite(T_comp)])
+
+
+def test_extract_several_consensus():
+    # This test is to further ensure that the function `ostinato`
+    # does not tamper with the original data.
+    Ts = [np.random.rand(n) for n in [256, 512, 1024]]
+    Ts_ref = [T.copy() for T in Ts]
+    Ts_comp = [T.copy() for T in Ts]
+
+    m = 20
+
+    k = 5  # Get the first `k` consensus motifs
+    for _ in range(k):
+        # Find consensus motif and its NN in each time series in Ts_comp
+        # Remove them from Ts_comp as well as Ts_ref, and assert that the
+        # the two time series are the same
+        radius, Ts_idx, subseq_idx = stumpy.aamp_ostinato(Ts_comp, m)
+        consensus_motif = Ts_comp[Ts_idx][subseq_idx : subseq_idx + m].copy()
+        for i in range(len(Ts_comp)):
+            if i == Ts_idx:
+                query_idx = subseq_idx
+            else:
+                query_idx = None
+
+            idx = np.argmin(
+                stumpy.core.mass(
+                    consensus_motif, Ts_comp[i], normalize=False, query_idx=query_idx
+                )
+            )
+            Ts_comp[i][idx : idx + m] = np.nan
+            Ts_ref[i][idx : idx + m] = np.nan
+
+            npt.assert_almost_equal(
+                Ts_ref[i][np.isfinite(Ts_ref[i])], Ts_comp[i][np.isfinite(Ts_comp[i])]
+            )
