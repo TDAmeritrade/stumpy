82bfeb19e06651910f599d1680f8aa8eead7f001
NimaSarajpoor <nimasarajpoor@gmail.com>
2024-09-22 23:42:30 -0400

    fix for black and flake8

--- utils.py
+++ utils.py
@@ -3,13 +3,13 @@ import ast
 
 def check_fastmath(decorator):
     """
-    For the given `decorator` node with type `ast.Call`, 
+    For the given `decorator` node with type `ast.Call`,
     return the value of the `fastmath` argument if it exists.
     Otherwise, return `None`.
     """
     fastmath_value = None
     for n in ast.iter_child_nodes(decorator):
-        if isinstance(n, ast.keyword) and n.arg == 'fastmath':
+        if isinstance(n, ast.keyword) and n.arg == "fastmath":
             if isinstance(n.value, ast.Constant):
                 fastmath_value = n.value.value
             elif isinstance(n.value, ast.Set):
@@ -21,10 +21,9 @@ def check_fastmath(decorator):
     return fastmath_value
 
 
-
 def check_njit(fd):
     """
-    For the given `fd` node with type `ast.FunctionDef`, 
+    For the given `fd` node with type `ast.FunctionDef`,
     return the node of the `njit` decorator if it exists.
     Otherwise, return `None`.
     """
@@ -38,7 +37,7 @@ def check_njit(fd):
             name = obj.attr
         elif isinstance(obj, ast.Subscript):
             name = obj.value.id
-        elif isinstance(obj, ast.Name):                
+        elif isinstance(obj, ast.Name):
             name = obj.id
         else:
             msg = f"The type {type(obj)} is not supported."
@@ -47,29 +46,29 @@ def check_njit(fd):
         if name == "njit":
             decorator_node = decorator
             break
-    
+
     return decorator_node
 
 
 def check_functions(filepath):
     """
-    For the given `filepath`, return the function names, 
-    whether the function is decorated with `@njit`, 
+    For the given `filepath`, return the function names,
+    whether the function is decorated with `@njit`,
     and the value of the `fastmath` argument if it exists
 
     Parameters
     ----------
     filepath : str
         The path to the file
-    
+
     Returns
     -------
     func_names : list
         List of function names
-    
+
     is_njit : list
         List of boolean values indicating whether the function is decorated with `@njit`
-    
+
     fastmath_value : list
         List of values of the `fastmath` argument if it exists
     """
@@ -77,19 +76,19 @@ def check_functions(filepath):
     with open(filepath, encoding="utf8") as f:
         file_contents = f.read()
     module = ast.parse(file_contents)
-    
+
     function_definitions = [
-        node for node in module.body if isinstance(node, ast.FunctionDef) 
+        node for node in module.body if isinstance(node, ast.FunctionDef)
     ]
 
     func_names = [fd.name for fd in function_definitions]
 
-    njit_nodes = [check_njit(fd) for fd in function_definitions]    
+    njit_nodes = [check_njit(fd) for fd in function_definitions]
     is_njit = [node is not None for node in njit_nodes]
 
-    fastmath_value = [None] * len(njit_nodes)
+    fastmath_values = [None] * len(njit_nodes)
     for i, node in enumerate(njit_nodes):
         if node is not None:
-            fastmath_value[i] = check_fastmath(node)
+            fastmath_values[i] = check_fastmath(node)
 
     return func_names, is_njit, fastmath_values
