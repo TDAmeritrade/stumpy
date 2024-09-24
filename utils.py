5cd3a35059e84e4b8f19ae85bcc24b822e0fcaba
NimaSarajpoor <nimasarajpoor@gmail.com>
2024-09-23 22:10:06 -0400

    add function to obtain callees of functions in a script

--- utils.py
+++ utils.py
@@ -92,3 +92,55 @@ def check_functions(filepath):
             fastmath_values[i] = check_fastmath(node)
 
     return func_names, is_njit, fastmath_values
+
+
+def _get_callees(node, all_functions):
+    for n in ast.iter_child_nodes(node):
+        if isinstance(n, ast.Call):
+            obj = n.func
+            if isinstance(obj, ast.Attribute):
+                name = obj.attr
+            elif isinstance(obj, ast.Subscript):
+                name = obj.value.id
+            elif isinstance(obj, ast.Name):
+                name = obj.id
+            else:
+                msg = f"The type {type(obj)} is not supported"
+                raise ValueError(msg)
+
+            all_functions.append(name)
+
+        _get_callees(n, all_functions)
+
+
+def get_all_callees(fd):
+    """
+    For a given node of type ast.FunctionDef, visit all of its child nodes,
+    and return a list of all of its callees
+    """
+    all_functions = []
+    _get_callees(fd, all_functions)
+
+    return all_functions
+
+
+def check_callees(filepath):
+    """
+    For the given `filepath`, return a dictionary with the key
+    being the function name and the value being a set of function names
+    that are called by the function
+    """
+    file_contents = ""
+    with open(filepath, encoding="utf8") as f:
+        file_contents = f.read()
+    module = ast.parse(file_contents)
+
+    function_definitions = [
+        node for node in module.body if isinstance(node, ast.FunctionDef)
+    ]
+
+    callees = {}
+    for fd in function_definitions:
+        callees[fd.name] = set(get_all_callees(fd))
+
+    return callees
