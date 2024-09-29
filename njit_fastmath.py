f548907d051d766bafb560411928b1b0bb9840a6
NimaSarajpoor <nimasarajpoor@gmail.com>
2024-09-28 23:02:20 -0400

    revise script to get two dictionarys with one being callee to callers map

--- njit_fastmath.py
+++ njit_fastmath.py
@@ -6,71 +6,67 @@ stumpy_path = pathlib.Path(__file__).parent / "stumpy"
 filepaths = sorted(f for f in pathlib.Path(stumpy_path).iterdir() if f.is_file())
 
 all_functions = {}
-callees = {}
 
 ignore = ["__init__.py", "__pycache__"]
 for filepath in filepaths:
-    name = filepath.name
-    if name not in ignore and str(filepath).endswith(".py"):
-        callees[name] = check_callees(filepath)
+    file_name = filepath.name
+    if file_name not in ignore and str(filepath).endswith(".py"):
+        prefix = file_name.replace(".py", "")
 
         func_names, is_njit, fastmath_values = check_functions(filepath)
-        all_functions[name] = {
+        func_names = [f"{prefix}.{fn}" for fn in func_names]
+
+        all_functions[file_name] = {
             "func_names": func_names,
             "is_njit": is_njit,
             "fastmath_values": fastmath_values,
         }
 
+all_stumpy_functions = set()
+for file_name, file_functions_metadata in all_functions.items():
+    all_stumpy_functions.update(file_functions_metadata["func_names"])
 
-stumpy_functions = set()
-for fname, func_data in all_functions.items():
-    prefix = fname.replace(".py", "")
-    stumpy_functions.update([prefix + "." + x for x in func_data["func_names"]])
-stumpy_functions = list(stumpy_functions)
-stumpy_functions_no_prefix = [x.split(".")[1] for x in stumpy_functions]
+all_stumpy_functions = list(all_stumpy_functions)
+all_stumpy_functions_no_prefix = [f.split(".")[-1] for f in all_stumpy_functions]
 
 
-# create  a dictionary where keys are function names in stumpy_functions, and the value
-# is a tuple, where the first element says whether the function is decorated with njit
-# and the second element is the list of callees
+# output 1: func_metadata
+func_metadata = {}
+for file_name, file_functions_metadata in all_functions.items():
+    for i, f in enumerate(file_functions_metadata["func_names"]):
+        is_njit = file_functions_metadata["is_njit"][i]
+        fastmath_value = file_functions_metadata["fastmath_values"][i]
+        func_metadata[f] = [is_njit, fastmath_value]
 
-callers_callees = {}
-for func_name in stumpy_functions:
-    callers_callees[func_name] = None
 
-    prefix, func = func_name.split(".")
-    fname = prefix + ".py"
+# output 2: func_callers
+func_callers = {}
+for f in func_metadata.keys():
+    func_callers[f] = []
 
-    idx = all_functions[fname]["func_names"].index(func)
-    is_njit = all_functions[fname]["is_njit"][idx]
-    fastmath_val = all_functions[fname]["fastmath_values"][idx]
+for filepath in filepaths:
+    file_name = filepath.name
+    if file_name in ignore or not str(filepath).endswith(".py"):
+        continue
 
-    callees_functions = callees[fname][func]
+    prefix = file_name.replace(".py", "")
+    callees = check_callees(filepath)
 
-    pruned_callees_functions = []
-    for callee in callees_functions:
-        if callee in all_functions[fname]["func_names"]:
-            pruned_callees_functions.append(prefix + "." + callee)
-        elif callee in stumpy_functions:
-            idx = stumpy_functions_no_prefix.index(callee)
-            pruned_callees_functions.append(stumpy_functions[idx])
-        else:
+    current_callers = set(callees.keys())
+    for caller, callee_set in callees.items():
+        s = list(callee_set.intersection(all_stumpy_functions_no_prefix))
+        if len(s) == 0:
             continue
 
-    callers_callees[func_name] = (is_njit, fastmath_val, pruned_callees_functions)
+        for c in s:
+            if c in current_callers:
+                c_name = prefix + "." + c
+            else:
+                idx = all_stumpy_functions_no_prefix.index(c)
+                c_name = all_stumpy_functions[idx]
+
+            func_callers[c_name].append(f"{prefix}.{caller}")
 
 
-# Create callees_callers dictionary using callers_callees dictionary
-callees_callers = {}
-for func_name, func_metadata in callers_callees.items():
-    callees_callers[func_name] = [func_metadata[0], func_metadata[1], []]
-
-
-for func_name, func_metadata in callers_callees.items():
-    for callee in func_metadata[2]:
-        callees_callers[callee][-1].append(func_name)
-
-
-for func_name, func_metadata in callees_callers.items():
-    callees_callers[func_name][2] = set(callees_callers[func_name][2])
-    callees_callers[func_name] = tuple(callees_callers[func_name])
+for f, callers in func_callers.items():
+    func_callers[f] = list(set(callers))
