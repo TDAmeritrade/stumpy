6264e942ad1262f016a131e53a4d5cbf5eda2a52
NimaSarajpoor <nimasarajpoor@gmail.com>
2025-01-03 23:44:48 -0500

    rename function

--- stumpy/fastmath.py
+++ stumpy/fastmath.py
@@ -1,13 +1,11 @@
-import ast
 import importlib
-import pathlib
 
 from stumpy import config
 
 
-def set_flag(module_name, func_name, flag=None):
+def _set(module_name, func_name, flag):
     """
-    Set a flag for a given function
+    Set fastmath flag for a given function
 
     Parameters
     ----------
@@ -15,27 +13,17 @@ def set_flag(module_name, func_name, flag=None):
         The module name
     func_name : str
         The function name
-    flag : str
-        The flag to set
+    flag : set or bool
+        The fastmath flag
 
     Returns
     -------
     None
     """
-    funcs_flags = get_default_fastmath(module_name)
-    if func_name not in funcs_flags.keys():
-        msg = f"The module `{module_name}` does not have a njit function `{func_name}`"
-        raise ValueError(msg)
-    default_flag = funcs_flags[func_name]
-
-    if flag is None:
-        flag = default_flag
-
-    if flag is not None:
-        module = importlib.import_module(f".{module_name}", package="stumpy")
-        func = getattr(module, func_name)
-        func.targetoptions["fastmath"] = flag
-        func.recompile()
+    module = importlib.import_module(f".{module_name}", package="stumpy")
+    func = getattr(module, func_name)
+    func.targetoptions["fastmath"] = flag
+    func.recompile()
 
     return
 
@@ -58,6 +46,6 @@ def _reset(module_name, func_name):
     key = module_name + "." + func_name
     key = "STUMPY_FASTMATH_" + key.upper()
     default_flag = config._STUMPY_DEFAULTS[key]
-    set_flag(module_name, func_name, default_flag)
+    _set(module_name, func_name, default_flag)
 
     return
