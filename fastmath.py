a90c8d4e2f87363ddfc643ce38281bfdeba5e0ef
NimaSarajpoor <nimasarajpoor@gmail.com>
2025-01-08 01:23:46 -0500

    use regex to find njit functions

--- fastmath.py
+++ fastmath.py
@@ -2,8 +2,44 @@
 
 import argparse
 import importlib
+import pathlib
+import re
 
-from stumpy.cache import get_njit_funcs
+
+def get_njit_funcs():
+    """
+    Retrieve a list of all njit functions
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    njit_funcs : list
+        A list of all njit functions, where each element is a tuple of the form
+        (module_name, func_name)
+    """
+    pattern = r"@njit.*?def\s+\w+\("
+
+    stumpy_path = pathlib.Path(__file__).parent / "stumpy"
+    filepaths = sorted(f for f in pathlib.Path(stumpy_path).iterdir() if f.is_file())
+
+    out = []
+    ignore = ["__init__.py", "__pycache__"]
+    for filepath in filepaths:
+        fname = filepath.name
+        if fname not in ignore and fname.endswith(".py"):
+            file_contents = ""
+            with open(filepath, encoding="utf8") as f:
+                file_contents = f.read()
+
+            matches = re.findall(pattern, file_contents, re.DOTALL)
+            for match in matches:
+                func_name = match.split("def ")[-1].split("(")[0]
+                out.append((fname.removesuffix(".py"), func_name))
+
+    return out
 
 
 def check_fastmath():
