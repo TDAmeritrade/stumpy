7e72a7f53cfc4ef8a87ae9f579634c80abaeacdc
NimaSarajpoor <nimasarajpoor@gmail.com>
2025-01-19 20:52:47 -0500

    Removed the trivial test function

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -8,37 +8,6 @@ if numba.config.DISABLE_JIT:
     pytest.skip("Skipping Tests JIT is disabled", allow_module_level=True)
 
 
-def test_cache_save():
-    def get_cache_fnames_ref():
-        cache.clear()
-        cache._enable()
-        stump(np.random.rand(10), 3)
-        cache_files = cache._get_cache()
-        cache.clear()
-        return cache_files
-
-    def get_cache_fnames_comp():
-        cache.clear()
-        cache.save()
-        stump(np.random.rand(10), 3)
-        cache_files = cache._get_cache()
-        cache.clear()
-        return cache_files
-
-    ref_cache_files = get_cache_fnames_ref()
-    comp_cache_files = get_cache_fnames_comp()
-
-    # check nbc files
-    ref_nbc = [fname for fname in ref_cache_files if fname.endswith(".nbc")]
-    comp_nbc = [fname for fname in comp_cache_files if fname.endswith(".nbc")]
-    assert sorted(ref_nbc) == sorted(comp_nbc)
-
-    # check nbi files
-    ref_nbi = [fname for fname in ref_cache_files if fname.endswith(".nbi")]
-    comp_nbi = [fname for fname in comp_cache_files if fname.endswith(".nbi")]
-    assert set(ref_nbi).issubset(comp_nbi)
-
-
 def test_cache_save_after_clear():
     T = np.random.rand(10)
     m = 3
