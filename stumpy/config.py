Fix fasthmath precision issue  (#1048)
* removed reassoc flag from fastmath

* Add reset feature to config

* Revised config value

* replaced fastmath flags with config var

* fixed format

* Removed bad f-string

* Replaced Raised with Returns in docstring

* Add second attempt for assertion

* minor change

* Add condition to avoid revising fastmath when JIT is disabled

* Removed support for input with type list to simplify function

* Refactored the recompile process

* removed blank lines

* fixed typo

* replaced hardcoded fastmath value with config var

* revised function

* renamed variable to improve readability

* fixed bug

* rename config to improve readability

* revise func clear

* revise func to recompile all njit functions

* Adapt to changes in test function

* add test

* resolve coverage

* resolve missing lines in coverage

* Add test function to improve coverage

* add fastmath module

* revise test function to use fastmath module

* fix minor issue

* minor change to improve readability

* Add fastmath default flags to config default

* add reset function

* rename function

* adapt recent changes in test function

* minor fixes

* Check if DISABLE_JIT before getting fastmath

* ignore lines for coverage check

* Editorial fix

* avoid .get(key) to get KeyError if it does not exist

* add function to save cache

* Add note to function

* fix format

* replace fastmath flag with config variable

* add test function to check backward compatibility

* skip test when JIT is disabled

* rename test function

* add conditional deprecation warning

* add test function to check if cache can be saved after cache._clear()

* remove old warning

* add test for cache._clear

* add wrapper around private functions

* Raise OSError when NUMBA JIT is disabled during cache save

* move warnings to public API

* fix warning message

* improved warning m

stumpy/config.py
@@ -2,15 +2,72 @@
 # Copyright 2019 TD Ameritrade. Released under the terms of the 3-Clause BSD license.
 # STUMPY is a trademark of TD Ameritrade IP Company, Inc. All rights reserved.
 
+import warnings
+
 import numpy as np
 
-STUMPY_THREADS_PER_BLOCK = 512
-STUMPY_MEAN_STD_NUM_CHUNKS = 1
-STUMPY_MEAN_STD_MAX_ITER = 10
-STUMPY_DENOM_THRESHOLD = 1e-14
-STUMPY_STDDEV_THRESHOLD = 1e-7
-STUMPY_P_NORM_THRESHOLD = 1e-14
-STUMPY_TEST_PRECISION = 5
-STUMPY_MAX_P_NORM_DISTANCE = np.finfo(np.float64).max
-STUMPY_MAX_DISTANCE = np.sqrt(STUMPY_MAX_P_NORM_DISTANCE)
-STUMPY_EXCL_ZONE_DENOM = 4
+_STUMPY_DEFAULTS = {
+    "STUMPY_THREADS_PER_BLOCK": 512,
+    "STUMPY_MEAN_STD_NUM_CHUNKS": 1,
+    "STUMPY_MEAN_STD_MAX_ITER": 10,
+    "STUMPY_DENOM_THRESHOLD": 1e-14,
+    "STUMPY_STDDEV_THRESHOLD": 1e-7,
+    "STUMPY_P_NORM_THRESHOLD": 1e-14,
+    "STUMPY_TEST_PRECISION": 5,
+    "STUMPY_MAX_P_NORM_DISTANCE": np.finfo(np.float64).max,
+    "STUMPY_MAX_DISTANCE": np.sqrt(np.finfo(np.float64).max),
+    "STUMPY_EXCL_ZONE_DENOM": 4,
+    "STUMPY_FASTMATH_TRUE": True,
+    "STUMPY_FASTMATH_FLAGS": {"nsz", "arcp", "contract", "afn", "reassoc"},
+}
+
+# In addition to these configuration variables, there exist config variables
+# that have the default value of the fastmath flag of the njit functions. The
+# name of this config variable has the following format:
+# STUMPY_FASTMATH_<module_name>.<function_name>
+# See __init__.py for more details
+
+STUMPY_THREADS_PER_BLOCK = _STUMPY_DEFAULTS["STUMPY_THREADS_PER_BLOCK"]
+STUMPY_MEAN_STD_NUM_CHUNKS = _STUMPY_DEFAULTS["STUMPY_MEAN_STD_NUM_CHUNKS"]
+STUMPY_MEAN_STD_MAX_ITER = _STUMPY_DEFAULTS["STUMPY_MEAN_STD_MAX_ITER"]
+STUMPY_DENOM_THRESHOLD = _STUMPY_DEFAULTS["STUMPY_DENOM_THRESHOLD"]
+STUMPY_STDDEV_THRESHOLD = _STUMPY_DEFAULTS["STUMPY_STDDEV_THRESHOLD"]
+STUMPY_P_NORM_THRESHOLD = _STUMPY_DEFAULTS["STUMPY_P_NORM_THRESHOLD"]
+STUMPY_TEST_PRECISION = _STUMPY_DEFAULTS["STUMPY_TEST_PRECISION"]
+STUMPY_MAX_P_NORM_DISTANCE = _STUMPY_DEFAULTS["STUMPY_MAX_P_NORM_DISTANCE"]
+STUMPY_MAX_DISTANCE = _STUMPY_DEFAULTS["STUMPY_MAX_DISTANCE"]
+STUMPY_EXCL_ZONE_DENOM = _STUMPY_DEFAULTS["STUMPY_EXCL_ZONE_DENOM"]
+STUMPY_FASTMATH_TRUE = _STUMPY_DEFAULTS["STUMPY_FASTMATH_TRUE"]
+STUMPY_FASTMATH_FLAGS = _STUMPY_DEFAULTS["STUMPY_FASTMATH_FLAGS"]
+
+
+def _reset(var=None):
+    """
+    Reset the value of a configuration variable(s) to their default value(s)
+
+    Parameters
+    ----------
+    var : str, default None
+        The name of the configuration variable. If None, then all
+        configuration variables are reset to their default values.
+
+    Returns
+    -------
+    None
+    """
+    config_vars = [
+        k for k, _ in globals().items() if k.isupper() and k.startswith("STUMPY")
+    ]
+
+    if var is None:
+        for config_var in config_vars:
+            globals()[config_var] = _STUMPY_DEFAULTS[config_var]
+    elif var in config_vars:
+        globals()[var] = _STUMPY_DEFAULTS[var]
+    else:  # pragma: no cover
+        msg = (
+            f"Configuration reset was skipped for unrecognized '_STUMPY_DEFAULT[{var}]'"
+        )
+        warnings.warn(msg)
+
+    return

stumpy/core.py
@@ -590,7 +590,7 @@ def check_window_size(m, max_size=None):
         raise ValueError(f"The window size must be less than or equal to {max_size}")
 
 
-@njit(fastmath=True)
+@njit(fastmath=config.STUMPY_FASTMATH_TRUE)
 def _sliding_dot_product(Q, T):
     """
     A Numba JIT-compiled implementation of the sliding window dot product.
@@ -658,7 +658,7 @@ def sliding_dot_product(Q, T):
 
 @njit(
     # "f8[:](f8[:], i8, b1[:])",
-    fastmath={"nsz", "arcp", "contract", "afn", "reassoc"}
+    fastmath=config.STUMPY_FASTMATH_FLAGS
 )
 def _welford_nanvar(a, w, a_subseq_isfinite):
     """
@@ -772,7 +772,7 @@ def welford_nanstd(a, w=None):
     return np.sqrt(np.clip(welford_nanvar(a, w), a_min=0, a_max=None))
 
 
-@njit(parallel=True, fastmath={"nsz", "arcp", "contract", "afn", "reassoc"})
+@njit(parallel=True, fastmath=config.STUMPY_FASTMATH_FLAGS)
 def _rolling_nanstd_1d(a, w):
     """
     A Numba JIT-compiled and parallelized function for computing the rolling standard
@@ -1043,7 +1043,7 @@ def compute_mean_std(T, m):
 
 @njit(
     # "f8(i8, f8, f8, f8, f8, f8)",
-    fastmath={"nsz", "arcp", "contract", "afn", "reassoc"}
+    fastmath=config.STUMPY_FASTMATH_FLAGS
 )
 def _calculate_squared_distance(
     m, QT, μ_Q, σ_Q, M_T, Σ_T, Q_subseq_isconstant, T_subseq_isconstant
@@ -1111,7 +1111,7 @@ def _calculate_squared_distance(
 
 @njit(
     # "f8[:](i8, f8[:], f8, f8, f8[:], f8[:])",
-    fastmath=True,
+    fastmath=config.STUMPY_FASTMATH_TRUE,
 )
 def _calculate_squared_distance_profile(
     m, QT, μ_Q, σ_Q, M_T, Σ_T, Q_subseq_isconstant, T_subseq_isconstant
@@ -1177,7 +1177,7 @@ def _calculate_squared_distance_profile(
 
 @njit(
     # "f8[:](i8, f8[:], f8, f8, f8[:], f8[:])",
-    fastmath=True,
+    fastmath=config.STUMPY_FASTMATH_TRUE,
 )
 def calculate_distance_profile(
     m, QT, μ_Q, σ_Q, M_T, Σ_T, Q_subseq_isconstant, T_subseq_isconstant
@@ -1230,7 +1230,7 @@ def calculate_distance_profile(
     return np.sqrt(D_squared)
 
 
-@njit(fastmath=True)
+@njit(fastmath=config.STUMPY_FASTMATH_TRUE)
 def _p_norm_distance_profile(Q, T, p=2.0):
     """
     A Numba JIT-compiled and parallelized function for computing the p-normalized
@@ -1506,7 +1506,7 @@ def mueen_calculate_distance_profile(Q, T):
 
 @njit(
     # "f8[:](f8[:], f8[:], f8[:], f8, f8, f8[:], f8[:])",
-    fastmath=True
+    fastmath=config.STUMPY_FASTMATH_TRUE
 )
 def _mass(Q, T, QT, μ_Q, σ_Q, M_T, Σ_T, Q_subseq_isconstant, T_subseq_isconstant):
     """
@@ -1979,7 +1979,7 @@ def _get_QT(start, T_A, T_B, m):
 
 @njit(
     # ["(f8[:], i8, i8)", "(f8[:, :], i8, i8)"],
-    fastmath=True
+    fastmath=config.STUMPY_FASTMATH_TRUE
 )
 def _apply_exclusion_zone(a, idx, excl_zone, val):
     """
@@ -2317,7 +2317,7 @@ def array_to_temp_file(a):
 
 @njit(
     # "i8[:](i8[:], i8, i8, i8)",
-    fastmath=True,
+    fastmath=config.STUMPY_FASTMATH_TRUE,
 )
 def _count_diagonal_ndist(diags, m, n_A, n_B):
     """
@@ -2356,7 +2356,7 @@ def _count_diagonal_ndist(diags, m, n_A, n_B):
 
 @njit(
     # "i8[:, :](i8[:], i8, b1)"
-    fastmath=True
+    fastmath=config.STUMPY_FASTMATH_TRUE
 )
 def _get_array_ranges(a, n_chunks, truncate):
     """
@@ -2405,7 +2405,7 @@ def _get_array_ranges(a, n_chunks, truncate):
 
 @njit(
     # "i8[:, :](i8, i8, b1)"
-    fastmath=True
+    fastmath=config.STUMPY_FASTMATH_TRUE
 )
 def _get_ranges(size, n_chunks, truncate):
     """
@@ -2516,7 +2516,7 @@ def rolling_isfinite(a, w):
     )
 
 
-@njit(parallel=True, fastmath={"nsz", "arcp", "contract", "afn", "reassoc"})
+@njit(parallel=True, fastmath=config.STUMPY_FASTMATH_FLAGS)
 def _rolling_isconstant(a, w):
     """
     Compute the rolling isconstant for 1-D array.
@@ -2853,7 +2853,7 @@ def _idx_to_mp(
     return P
 
 
-@njit(fastmath=True)
+@njit(fastmath=config.STUMPY_FASTMATH_TRUE)
 def _total_diagonal_ndists(tile_lower_diag, tile_upper_diag, tile_height, tile_width):
     """
     Count the total number of distances covered by a range of diagonals
@@ -3258,7 +3258,7 @@ def _select_P_ABBA_value(P_ABBA, k, custom_func=None):
     return MPdist
 
 
-@njit(fastmath={"nsz", "arcp", "contract", "afn", "reassoc"})
+@njit(fastmath=config.STUMPY_FASTMATH_FLAGS)
 def _merge_topk_PI(PA, PB, IA, IB):
     """
     Merge two top-k matrix profiles `PA` and `PB`, and update `PA` (in place).
@@ -3331,7 +3331,7 @@ def _merge_topk_PI(PA, PB, IA, IB):
             IA[i] = tmp_I
 
 
-@njit(fastmath={"nsz", "arcp", "contract", "afn", "reassoc"})
+@njit(fastmath=config.STUMPY_FASTMATH_FLAGS)
 def _merge_topk_ρI(ρA, ρB, IA, IB):
     """
     Merge two top-k pearson profiles `ρA` and `ρB`, and update `ρA` (in place).
@@ -3405,7 +3405,7 @@ def _merge_topk_ρI(ρA, ρB, IA, IB):
             IA[i] = tmp_I
 
 
-@njit(fastmath={"nsz", "arcp", "contract", "afn", "reassoc"})
+@njit(fastmath=config.STUMPY_FASTMATH_FLAGS)
 def _shift_insert_at_index(a, idx, v, shift="right"):
     """
     If `shift=right` (default), all elements in `a[idx:]` are shifted to the right by
@@ -3981,7 +3981,7 @@ def _mdl(disc_subseqs, disc_neighbors, S, n_bit=8):
 
 @njit(
     # "(i8, i8, f8[:, :], f8[:], i8, f8[:, :], i8[:, :], f8)",
-    fastmath={"nsz", "arcp", "contract", "afn", "reassoc"},
+    fastmath=config.STUMPY_FASTMATH_FLAGS,
 )
 def _compute_multi_PI(d, idx, D, D_prime, range_start, P, I, p=2.0):
     """
@@ -4381,7 +4381,7 @@ def get_ray_nworkers(ray_client):
     return int(ray_client.cluster_resources().get("CPU"))
 
 
-@njit(fastmath={"nsz", "arcp", "contract", "afn", "reassoc"})
+@njit(fastmath=config.STUMPY_FASTMATH_FLAGS)
 def _update_incremental_PI(D, P, I, excl_zone, n_appended=0):
     """
     Given the 1D array distance profile, `D`, of the last subsequence of T,