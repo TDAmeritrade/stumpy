df9381b614fc475c7224d50095d73e473bb28b1c
NimaSarajpoor <nimasarajpoor@gmail.com>
2025-01-28 01:06:35 -0500

    fix fastmath for aamp._aamp

--- stumpy/aamp.py
+++ stumpy/aamp.py
@@ -186,7 +186,7 @@ def _compute_diagonal(
 @njit(
     # "(f8[:], f8[:], i8, b1[:], b1[:], i8[:], b1, i8)",
     parallel=True,
-    fastmath=config.STUMPY_FASTMATH_TRUE,
+    fastmath=config.STUMPY_FASTMATH_FLAGS,
 )
 def _aamp(
     T_A,
