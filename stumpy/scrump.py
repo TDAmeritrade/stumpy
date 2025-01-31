c895270fe1fee90f1b60372f441cc2b71061233c
NimaSarajpoor <nimasarajpoor@gmail.com>
2025-01-30 20:13:22 -0500

    fix fastmath for scrump._prescrump

--- stumpy/scrump.py
+++ stumpy/scrump.py
@@ -384,7 +384,7 @@ def _compute_PI(
     # "(f8[:], f8[:], i8, f8[:], f8[:], f8[:], f8[:], f8[:], i8, i8, f8[:], f8[:],"
     # "i8[:], optional(i8))",
     parallel=True,
-    fastmath=config.STUMPY_FASTMATH_TRUE,
+    fastmath=config.STUMPY_FASTMATH_FLAGS,
 )
 def _prescrump(
     T_A,
